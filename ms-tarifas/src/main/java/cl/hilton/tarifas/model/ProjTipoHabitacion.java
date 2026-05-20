7abda64cc22822f790f5508e29cf058d780e3d90
lukasriquelme <luka.riquelmem@duocuc.cl>
2026-05-20 19:53:26 -0400

    ProjTipoHabitacion.java
    Se crea entidad JPA ProjTipoHabitacion

---
+++ ms-tarifas/src/main/java/cl/hilton/tarifas/model/ProjTipoHabitacion.java
@@ -0,0 +1,33 @@
+package cl.hilton.tarifas.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "proj_tipo_habitacion")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProjTipoHabitacion {
+
+    @Id
+    @Column(name = "codigo", nullable = false, length = 40)
+    private String codigo;
+
+    @Column(name = "descripcion", length = 100)
+    private String descripcion;
+
+    @Column(name = "capacidad_max", nullable = false)
+    private Short capacidadMax;
+
+    @Column(name = "actualizado_en", nullable = false)
+    private OffsetDateTime actualizadoEn;
+
+    @OneToMany(mappedBy = "tipoHabitacion")
+    private List<Tarifa> tarifas;
+}
