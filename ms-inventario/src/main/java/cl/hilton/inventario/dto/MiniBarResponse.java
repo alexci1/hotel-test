38787eb5c19d6b933d9b6fa6709cd31a8b671ba1
constanza <co.barrag@duocuc.cl>
2026-05-22 14:03:53 -0400

    pruebaxd
    id falta el d

--- ms-inventario/src/main/java/cl/hilton/inventario/dto/MiniBarResponse.java
+++ ms-inventario/src/main/java/cl/hilton/inventario/dto/MiniBarResponse.java
@@ -11,7 +11,7 @@ import java.math.BigDecimal;
 @Builder
 public class MinibarResponse {
 
-    private Long id;
+    private Long i;
     private String numeroHabitacion;
     private String codigoProducto;
     private String nombreProducto;
