546ceda6b52c6c03df5355f15d05bf25b4c41811
constanza <co.barrag@duocuc.cl>
2026-05-22 14:05:12 -0400

    ahora si
    jeje

--- ms-inventario/src/main/java/cl/hilton/inventario/dto/MiniBarResponse.java
+++ ms-inventario/src/main/java/cl/hilton/inventario/dto/MiniBarResponse.java
@@ -11,7 +11,7 @@ import java.math.BigDecimal;
 @Builder
 public class MinibarResponse {
 
-    private Long i;
+    private Long id;
     private String numeroHabitacion;
     private String codigoProducto;
     private String nombreProducto;
