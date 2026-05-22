63b16f9530d1e4952fbe8efd8310329117573a2c
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-22 14:07:49 -0400

    Update MiniBarResponse.java

--- ms-inventario/src/main/java/cl/hilton/inventario/dto/MiniBarResponse.java
+++ ms-inventario/src/main/java/cl/hilton/inventario/dto/MiniBarResponse.java
@@ -9,7 +9,7 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class MinibarResponse {
+public class MiniBarResponse {
 
     private Long id;
     private String numeroHabitacion;
