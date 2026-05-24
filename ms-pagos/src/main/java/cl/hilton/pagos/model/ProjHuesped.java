f97da63f544f8b13a3c8524e3f1f797a680ea210
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-24 19:25:37 -0400

    Actualiza JPA ProjHuesped desde SQL pagos

--- ms-pagos/src/main/java/cl/hilton/pagos/model/ProjHuesped.java
+++ ms-pagos/src/main/java/cl/hilton/pagos/model/ProjHuesped.java
@@ -1,13 +1,23 @@
 package cl.hilton.pagos.model;
 
-import jakarta.persistence.*;
-import lombok.*;
-
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Entity
-@Table(name = "proj_huesped")
+@Table(name = "proj_huespedes")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -22,7 +32,10 @@ public class ProjHuesped {
     @Column(name = "nombre_completo", nullable = false, length = 100)
     private String nombreCompleto;
 
-    @Builder.Default
+    @Column(name = "actualizado_en", nullable = false)
+    private LocalDate actualizadoEn;
+
     @OneToMany(mappedBy = "huesped", fetch = FetchType.LAZY)
+    @Builder.Default
     private List<Factura> facturas = new ArrayList<>();
 }
