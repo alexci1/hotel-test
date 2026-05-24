c0d763c1456c5b841d3576fda94112dfc7e7dc58
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-24 19:08:45 -0400

    Se arregla housekeeping
    Archivos nuevamente creados

---
+++ ms-housekeeping/src/main/java/cl/hilton/housekeeping/HotelHousekeepingApplication.java
@@ -0,0 +1,12 @@
+package cl.hilton.housekeeping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelHousekeepingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelHousekeepingApplication.class, args);
+    }
+}
