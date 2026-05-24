00699245d00ec0a190da12777979b199a0cd1081
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-24 19:37:00 -0400

    Actualiza JPA Mesa desde SQL restaurante

--- ms-restaurante/src/main/java/cl/hilton/restaurante/model/Mesa.java
+++ ms-restaurante/src/main/java/cl/hilton/restaurante/model/Mesa.java
@@ -1,20 +1,27 @@
 package cl.hilton.restaurante.model;
 
-
-import jakarta.persistence.*;
-import lombok.*;
-
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Entity
 @Table(
-    name = "mesa",
+    name = "mesas",
     uniqueConstraints = {
-        @UniqueConstraint(
-            name = "uk_mesa_numero_mesa",
-            columnNames = "numero_mesa"
-        )
+        @UniqueConstraint(name = "uk_mesas_numero_mesa", columnNames = "numero_mesa")
     }
 )
 @Getter
@@ -26,14 +33,14 @@ public class Mesa {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
+    @Column(name = "id", nullable = false)
     private Integer id;
 
-    @Column(name = "numero_mesa", nullable = false, length = 10)
+    @Column(name = "numero_mesa", nullable = false, length = 10, unique = true)
     private String numeroMesa;
 
     @Column(name = "capacidad", nullable = false)
-    private Short capacidad;
+    private Integer capacidad;
 
     @Column(name = "zona", nullable = false, length = 40)
     private String zona;
@@ -45,4 +52,3 @@ public class Mesa {
     @Builder.Default
     private List<Pedido> pedidos = new ArrayList<>();
 }
-
