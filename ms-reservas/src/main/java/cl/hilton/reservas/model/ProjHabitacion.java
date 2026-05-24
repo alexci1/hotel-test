f198728c655ce42094bf5e5a6761cb6024d3ad19
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-24 19:32:54 -0400

    Actualiza JPA ProjHabitacion desde SQL reservas

--- ms-reservas/src/main/java/cl/hilton/reservas/model/ProjHabitacion.java
+++ ms-reservas/src/main/java/cl/hilton/reservas/model/ProjHabitacion.java
@@ -1,12 +1,22 @@
 package cl.hilton.reservas.model;
 
-import jakarta.persistence.*;
-import lombok.*;
-
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-@Table(name = "proj_habitacion")
+@Table(name = "proj_habitaciones")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -26,4 +36,12 @@ public class ProjHabitacion {
 
     @Column(name = "actualizado_en", nullable = false)
     private LocalDate actualizadoEn;
+
+    @OneToMany(mappedBy = "habitacion")
+    @Builder.Default
+    private List<Reserva> reservas = new ArrayList<>();
+
+    @OneToMany(mappedBy = "habitacion")
+    @Builder.Default
+    private List<Disponibilidad> disponibilidades = new ArrayList<>();
 }
