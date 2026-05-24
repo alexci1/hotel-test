7ce583a3039f029e022714fc2582b0b548f27fca
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-24 19:17:35 -0400

    Actualiza JPA ProjHabitacion desde SQL inventario

--- ms-inventario/src/main/java/cl/hilton/inventario/model/ProjHabitacion.java
+++ ms-inventario/src/main/java/cl/hilton/inventario/model/ProjHabitacion.java
@@ -1,14 +1,22 @@
 package cl.hilton.inventario.model;
 
-import jakarta.persistence.*;
-import lombok.*;
-
-import java.time.OffsetDateTime;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Entity
-@Table(name = "proj_habitacion")
+@Table(name = "proj_habitaciones")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -24,10 +32,9 @@ public class ProjHabitacion {
     private String tipo;
 
     @Column(name = "actualizado_en", nullable = false)
-    private OffsetDateTime actualizadoEn;
+    private LocalDate actualizadoEn;
 
     @OneToMany(mappedBy = "habitacion")
     @Builder.Default
     private List<Minibar> minibares = new ArrayList<>();
 }
-
