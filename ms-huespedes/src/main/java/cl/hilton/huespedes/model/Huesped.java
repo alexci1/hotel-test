29a77a6569342b05a97f72ed93a14234aee027d2
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-24 19:11:21 -0400

    Crea JPA Huesped desde SQL huespedes

---
+++ ms-huespedes/src/main/java/cl/hilton/huespedes/model/Huesped.java
@@ -0,0 +1,62 @@
+package cl.hilton.huespedes.model;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(
+    name = "huespedes",
+    uniqueConstraints = {
+        @UniqueConstraint(name = "uk_huespedes_email", columnNames = "email")
+    }
+)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Huesped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "email", nullable = false, length = 120, unique = true)
+    private String email;
+
+    @Column(name = "nombre_completo", nullable = false, length = 100)
+    private String nombreCompleto;
+
+    @Column(name = "telefono", length = 20)
+    private String telefono;
+
+    @Column(name = "activo", nullable = false)
+    private Boolean activo;
+
+    @Column(name = "creado_en", nullable = false)
+    private LocalDate creadoEn;
+
+    @OneToMany(mappedBy = "huesped")
+    @Builder.Default
+    private List<Documento> documentos = new ArrayList<>();
+
+    @OneToOne(mappedBy = "huesped")
+    private Preferencia preferencia;
+}
