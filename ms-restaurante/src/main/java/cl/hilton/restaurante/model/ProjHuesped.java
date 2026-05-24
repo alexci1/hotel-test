cd79b48c50ae41efa061c7c9de5ef946b24f7168
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-24 19:35:43 -0400

    Actualiza JPA ProjHuesped desde SQL restaurante

--- ms-restaurante/src/main/java/cl/hilton/restaurante/model/ProjHuesped.java
+++ ms-restaurante/src/main/java/cl/hilton/restaurante/model/ProjHuesped.java
@@ -1,15 +1,22 @@
 package cl.hilton.restaurante.model;
 
-
-import jakarta.persistence.*;
-import lombok.*;
-
-import java.time.OffsetDateTime;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Entity
-@Table(name = "proj_huesped")
+@Table(name = "proj_huespedes")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -28,7 +35,7 @@ public class ProjHuesped {
     private String numeroHabitacion;
 
     @Column(name = "actualizado_en", nullable = false)
-    private OffsetDateTime actualizadoEn;
+    private LocalDate actualizadoEn;
 
     @OneToMany(mappedBy = "huesped")
     @Builder.Default
