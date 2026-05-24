f85529d2ac8f05e2d7952768fddc03361965c100
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-24 19:33:41 -0400

    Actualiza JPA Cancelacion desde SQL reservas

--- ms-reservas/src/main/java/cl/hilton/reservas/model/Cancelacion.java
+++ ms-reservas/src/main/java/cl/hilton/reservas/model/Cancelacion.java
@@ -1,13 +1,30 @@
 package cl.hilton.reservas.model;
 
-import jakarta.persistence.*;
-import lombok.*;
-
-import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Entity
-@Table(name = "cancelacion")
+@Table(
+    name = "cancelaciones",
+    uniqueConstraints = {
+        @UniqueConstraint(name = "uk_cancelaciones_codigo_reserva", columnNames = "codigo_reserva")
+    }
+)
 @Getter
 @Setter
 @NoArgsConstructor
@@ -17,9 +34,10 @@ public class Cancelacion {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
     private Long id;
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "codigo_reserva", referencedColumnName = "codigo_reserva", nullable = false, unique = true)
     private Reserva reserva;
 
@@ -33,5 +51,5 @@ public class Cancelacion {
     private LocalDate canceladoEn;
 
     @Column(name = "penalidad_usd", nullable = false)
-    private BigDecimal penalidadUsd;
+    private Integer penalidadUsd;
 }
