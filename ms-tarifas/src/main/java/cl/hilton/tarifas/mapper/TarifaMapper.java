3f187f5d41dc6e78123eadb1744919e0408bd5ad
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-25 14:03:11 -0400

    fix: alinear tarifa mapper con relaciones jpa

--- ms-tarifas/src/main/java/cl/hilton/tarifas/mapper/TarifaMapper.java
+++ ms-tarifas/src/main/java/cl/hilton/tarifas/mapper/TarifaMapper.java
@@ -2,17 +2,24 @@ package cl.hilton.tarifas.mapper;
 
 import cl.hilton.tarifas.dto.TarifaRequest;
 import cl.hilton.tarifas.dto.TarifaResponse;
+import cl.hilton.tarifas.model.ProjTipoHabitacion;
 import cl.hilton.tarifas.model.Tarifa;
+import cl.hilton.tarifas.model.Temporada;
 
 public class TarifaMapper {
 
-    public static Tarifa toEntity(TarifaRequest request) {
+    public static Tarifa toEntity(
+            TarifaRequest request,
+            Temporada temporada,
+            ProjTipoHabitacion tipoHabitacion
+    ) {
 
         return Tarifa.builder()
-                .codigo(request.getCodigo())
-                .precioBaseUsd(request.getPrecioBaseUsd())
-                .activa(request.getActiva())
+                .temporada(temporada)
+                .tipoHabitacion(tipoHabitacion)
+                .precioNocheUsd(request.getPrecioNocheUsd())
                 .incluyeDesayuno(request.getIncluyeDesayuno())
+                .activa(request.getActiva())
                 .build();
     }
 
@@ -21,10 +28,11 @@ public class TarifaMapper {
         TarifaResponse response = new TarifaResponse();
 
         response.setId(tarifa.getId());
-        response.setCodigo(tarifa.getCodigo());
-        response.setPrecioBaseUsd(tarifa.getPrecioBaseUsd());
-        response.setActiva(tarifa.getActiva());
+        response.setCodigoTemporada(tarifa.getTemporada().getCodigo());
+        response.setCodigoTipoHabitacion(tarifa.getTipoHabitacion().getCodigo());
+        response.setPrecioNocheUsd(tarifa.getPrecioNocheUsd());
         response.setIncluyeDesayuno(tarifa.getIncluyeDesayuno());
+        response.setActiva(tarifa.getActiva());
 
         return response;
     }
