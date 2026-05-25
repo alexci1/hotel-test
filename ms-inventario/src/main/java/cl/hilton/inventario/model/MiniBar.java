b3061c73d616b6c10af262d0fef39dc6cda26cb2
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-25 14:06:29 -0400

    fix: renombrar archivo MiniBar

---
+++ ms-inventario/src/main/java/cl/hilton/inventario/model/MiniBar.java
@@ -0,0 +1,51 @@
+package cl.hilton.inventario.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(
+    name = "minibares",
+    uniqueConstraints = {
+        @UniqueConstraint(name = "uq_minibares", columnNames = {"numero_habitacion", "codigo_producto"})
+    }
+)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MiniBar {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "numero_habitacion", referencedColumnName = "numero_habitacion", nullable = false)
+    private ProjHabitacion habitacion;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "codigo_producto", referencedColumnName = "codigo_producto", nullable = false)
+    private Producto producto;
+
+    @Column(name = "cantidad", nullable = false)
+    private Integer cantidad;
+
+    @Column(name = "precio_unit_usd", nullable = false)
+    private Integer precioUnitUsd;
+}
