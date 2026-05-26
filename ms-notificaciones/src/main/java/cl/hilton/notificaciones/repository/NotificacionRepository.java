2226f9d5b79978f5d53eb4b603eb723a43567945
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-26 15:06:36 -0400

    Update NotificacionRepository queries

--- ms-notificaciones/src/main/java/cl/hilton/notificaciones/repository/NotificacionRepository.java
+++ ms-notificaciones/src/main/java/cl/hilton/notificaciones/repository/NotificacionRepository.java
@@ -1,6 +1,7 @@
 package cl.hilton.notificaciones.repository;
 
-import java.util.Optional;
+import java.time.LocalDate;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,7 +11,11 @@ import cl.hilton.notificaciones.model.Notificacion;
 @Repository
 public interface NotificacionRepository extends JpaRepository<Notificacion, Long> {
 
-    Optional<Notificacion> findByEventoOrigen(String eventoOrigen);
+    List<Notificacion> findByEventoOrigen(String eventoOrigen);
 
-    boolean existsByEventoOrigen(String eventoOrigen);
+    List<Notificacion> findByPlantillaCodigo(String codigoPlantilla);
+
+    List<Notificacion> findByHuespedEmail(String emailHuesped);
+
+    List<Notificacion> findByCreadoEn(LocalDate creadoEn);
 }
