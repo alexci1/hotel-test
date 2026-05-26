e539cd7d221c6f8d8e3bc15c249a4fe62d24cfeb
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-26 15:34:41 -0400

    Update FacturaRepository queries

--- ms-pagos/src/main/java/cl/hilton/pagos/repository/FacturaRepository.java
+++ ms-pagos/src/main/java/cl/hilton/pagos/repository/FacturaRepository.java
@@ -1,5 +1,7 @@
 package cl.hilton.pagos.repository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,4 +13,16 @@ import cl.hilton.pagos.model.Factura;
 public interface FacturaRepository extends JpaRepository<Factura, Long> {
 
     Optional<Factura> findByNumeroFactura(String numeroFactura);
+
+    boolean existsByNumeroFactura(String numeroFactura);
+
+    Optional<Factura> findByReservaCodigoReserva(String codigoReserva);
+
+    boolean existsByReservaCodigoReserva(String codigoReserva);
+
+    List<Factura> findByHuespedEmail(String emailHuesped);
+
+    List<Factura> findByEstado(String estado);
+
+    List<Factura> findByEmitidaEn(LocalDate emitidaEn);
 }
