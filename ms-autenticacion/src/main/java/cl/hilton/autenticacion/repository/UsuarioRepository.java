2ac7e84e630f4bfd78b88378d9c5dfec02b8e5bc
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-26 15:44:58 -0400

    Update UsuarioRepository queries

--- ms-autenticacion/src/main/java/cl/hilton/autenticacion/repository/UsuarioRepository.java
+++ ms-autenticacion/src/main/java/cl/hilton/autenticacion/repository/UsuarioRepository.java
@@ -1,5 +1,7 @@
 package cl.hilton.autenticacion.repository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,9 +10,17 @@ import org.springframework.stereotype.Repository;
 import cl.hilton.autenticacion.model.Usuario;
 
 @Repository
-public interface UsuarioRepository extends JpaRepository<Usuario,Long>{
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
     Optional<Usuario> findByEmail(String email);
 
     boolean existsByEmail(String email);
-} 
+
+    List<Usuario> findByRolCodigo(String rolCodigo);
+
+    List<Usuario> findByActivo(Boolean activo);
+
+    List<Usuario> findByCreadoEn(LocalDate creadoEn);
+
+    List<Usuario> findByUltimoAcceso(LocalDate ultimoAcceso);
+}
