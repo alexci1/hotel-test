b735b052e7ce3f61d78b600f5235ee07ce6bced3
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-26 15:45:14 -0400

    Update SesionRepository queries

--- ms-autenticacion/src/main/java/cl/hilton/autenticacion/repository/SesionRepository.java
+++ ms-autenticacion/src/main/java/cl/hilton/autenticacion/repository/SesionRepository.java
@@ -1,5 +1,7 @@
 package cl.hilton.autenticacion.repository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,9 +10,19 @@ import org.springframework.stereotype.Repository;
 import cl.hilton.autenticacion.model.Sesion;
 
 @Repository
-public interface SesionRepository extends JpaRepository<Sesion,Long>{
-    
+public interface SesionRepository extends JpaRepository<Sesion, Long> {
+
     Optional<Sesion> findByTokenHash(String tokenHash);
 
     boolean existsByTokenHash(String tokenHash);
+
+    Optional<Sesion> findByUsuarioEmail(String usuarioEmail);
+
+    boolean existsByUsuarioEmail(String usuarioEmail);
+
+    List<Sesion> findByExpiraEn(LocalDate expiraEn);
+
+    List<Sesion> findByInvalidada(Boolean invalidada);
+
+    List<Sesion> findByInvalidadaFalse();
 }
