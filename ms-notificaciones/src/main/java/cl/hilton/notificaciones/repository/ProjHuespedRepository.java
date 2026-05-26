eca452ee19b4ca70ae5cb9cebf9f9f865594914e
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-26 15:06:26 -0400

    Update ProjHuespedRepository queries

--- ms-notificaciones/src/main/java/cl/hilton/notificaciones/repository/ProjHuespedRepository.java
+++ ms-notificaciones/src/main/java/cl/hilton/notificaciones/repository/ProjHuespedRepository.java
@@ -1,5 +1,6 @@
 package cl.hilton.notificaciones.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,4 +12,8 @@ import cl.hilton.notificaciones.model.ProjHuesped;
 public interface ProjHuespedRepository extends JpaRepository<ProjHuesped, String> {
 
     Optional<ProjHuesped> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    List<ProjHuesped> findByNombreCompletoContainingIgnoreCase(String nombreCompleto);
 }
