3b828e7889e10af8a50a288ef1930ea3d0c687d1
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-28 15:08:25 -0400

    Enable Feign clients in inventario

--- ms-inventario/src/main/java/cl/hilton/inventario/HotelInventarioApplication.java
+++ ms-inventario/src/main/java/cl/hilton/inventario/HotelInventarioApplication.java
@@ -2,8 +2,10 @@ package cl.hilton.inventario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class HotelInventarioApplication {
 
 	public static void main(String[] args) {
