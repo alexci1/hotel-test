c0435d9168ff2833d212c2e81b4a96c8da7436da
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-28 16:58:46 -0400

    Remove common dependency from Notificacion event producer

--- ms-notificaciones/src/main/java/cl/hilton/notificaciones/event/NotificacionEventProducer.java
+++ ms-notificaciones/src/main/java/cl/hilton/notificaciones/event/NotificacionEventProducer.java
@@ -1,14 +1,9 @@
 package cl.hilton.notificaciones.event;
 
-import cl.hilton.common.event.NotificacionCreatedEvent;
-import cl.hilton.common.event.NotificacionDeletedEvent;
-import cl.hilton.common.event.NotificacionUpdatedEvent;
-import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class NotificacionEventProducer {
 
     public static final String NOTIFICACION_CREATED_TOPIC = "notificacion.created";
@@ -17,15 +12,19 @@ public class NotificacionEventProducer {
 
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
-    public void publishCreated(NotificacionCreatedEvent event) {
-        kafkaTemplate.send(NOTIFICACION_CREATED_TOPIC, event.getCodigoPlantilla(), event);
+    public NotificacionEventProducer(KafkaTemplate<String, Object> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
     }
 
-    public void publishUpdated(NotificacionUpdatedEvent event) {
-        kafkaTemplate.send(NOTIFICACION_UPDATED_TOPIC, event.getCodigoPlantilla(), event);
+    public void publishCreated(String key, Object event) {
+        kafkaTemplate.send(NOTIFICACION_CREATED_TOPIC, key, event);
     }
 
-    public void publishDeleted(NotificacionDeletedEvent event) {
-        kafkaTemplate.send(NOTIFICACION_DELETED_TOPIC, event.getCodigoPlantilla(), event);
+    public void publishUpdated(String key, Object event) {
+        kafkaTemplate.send(NOTIFICACION_UPDATED_TOPIC, key, event);
+    }
+
+    public void publishDeleted(String key, Object event) {
+        kafkaTemplate.send(NOTIFICACION_DELETED_TOPIC, key, event);
     }
 }
