a94362302d860f1145cabb6bc4598d13d726f968
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-28 15:16:48 -0400

    Enable Feign clients in notificaciones

--- ms-notificaciones/src/main/java/cl/hilton/notificaciones/HotelNotificacionesApplication.java
+++ ms-notificaciones/src/main/java/cl/hilton/notificaciones/HotelNotificacionesApplication.java
@@ -2,8 +2,10 @@ package cl.hilton.notificaciones;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class HotelNotificacionesApplication {
 
 	public static void main(String[] args) {
