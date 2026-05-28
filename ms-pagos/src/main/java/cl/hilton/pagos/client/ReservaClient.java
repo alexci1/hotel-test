5958e1b03f5b3438dd25c06e75a24513688e77c2
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-28 16:02:59 -0400

    Update pagos ReservaClient

--- ms-pagos/src/main/java/cl/hilton/pagos/client/ReservaClient.java
+++ ms-pagos/src/main/java/cl/hilton/pagos/client/ReservaClient.java
@@ -1,5 +1,6 @@
 package cl.hilton.pagos.client;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.cloud.openfeign.FeignClient;
@@ -9,9 +10,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "ms-reservas")
 public interface ReservaClient {
 
+    @GetMapping("/reservas")
+    List<Map<String, Object>> listar();
+
     @GetMapping("/reservas/{id}")
     Map<String, Object> buscarPorId(@PathVariable("id") Long id);
 
     @GetMapping("/reservas/codigo/{codigoReserva}")
     Map<String, Object> buscarPorCodigoReserva(@PathVariable("codigoReserva") String codigoReserva);
+
+    @GetMapping("/reservas/estado/{estado}")
+    List<Map<String, Object>> buscarPorEstado(@PathVariable("estado") String estado);
 }
