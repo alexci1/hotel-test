81f33dbe7522a504fc41ca2cef2b4675eeb06c58
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-28 16:50:14 -0400

    Simplify MesaRestaurante event producer imports

--- ms-restaurante/src/main/java/cl/hilton/restaurante/event/MesaRestauranteEventProducer.java
+++ ms-restaurante/src/main/java/cl/hilton/restaurante/event/MesaRestauranteEventProducer.java
@@ -1,31 +1,26 @@
 package cl.hilton.restaurante.event;
 
-import cl.hilton.common.event.MesaRestauranteCreatedEvent;
-import cl.hilton.common.event.MesaRestauranteDeletedEvent;
-import cl.hilton.common.event.MesaRestauranteUpdatedEvent;
-import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class MesaRestauranteEventProducer {
-
-    public static final String MESA_CREATED_TOPIC = "mesa-restaurante.created";
-    public static final String MESA_UPDATED_TOPIC = "mesa-restaurante.updated";
-    public static final String MESA_DELETED_TOPIC = "mesa-restaurante.deleted";
 
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
-    public void publishCreated(MesaRestauranteCreatedEvent event) {
-        kafkaTemplate.send(MESA_CREATED_TOPIC, event.getNumeroMesa(), event);
+    public MesaRestauranteEventProducer(KafkaTemplate<String, Object> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
     }
 
-    public void publishUpdated(MesaRestauranteUpdatedEvent event) {
-        kafkaTemplate.send(MESA_UPDATED_TOPIC, event.getNumeroMesa(), event);
+    public void publishCreated(String key, Object event) {
+        kafkaTemplate.send("mesa-restaurante.created", key, event);
     }
 
-    public void publishDeleted(MesaRestauranteDeletedEvent event) {
-        kafkaTemplate.send(MESA_DELETED_TOPIC, event.getNumeroMesa(), event);
+    public void publishUpdated(String key, Object event) {
+        kafkaTemplate.send("mesa-restaurante.updated", key, event);
+    }
+
+    public void publishDeleted(String key, Object event) {
+        kafkaTemplate.send("mesa-restaurante.deleted", key, event);
     }
 }
