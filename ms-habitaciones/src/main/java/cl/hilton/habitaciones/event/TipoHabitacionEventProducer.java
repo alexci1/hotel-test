88e65b4eef7cd685fe8450c6b7bf353ec06f52b1
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-28 16:37:37 -0400

    Remove common dependency from TipoHabitacion event producer

--- ms-habitaciones/src/main/java/cl/hilton/habitaciones/event/TipoHabitacionEventProducer.java
+++ ms-habitaciones/src/main/java/cl/hilton/habitaciones/event/TipoHabitacionEventProducer.java
@@ -1,14 +1,9 @@
 package cl.hilton.habitaciones.event;
 
-import cl.hilton.common.event.TipoHabitacionCreatedEvent;
-import cl.hilton.common.event.TipoHabitacionDeletedEvent;
-import cl.hilton.common.event.TipoHabitacionUpdatedEvent;
-import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class TipoHabitacionEventProducer {
 
     public static final String TIPO_HABITACION_CREATED_TOPIC = "tipo-habitacion.created";
@@ -17,15 +12,19 @@ public class TipoHabitacionEventProducer {
 
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
-    public void publishCreated(TipoHabitacionCreatedEvent event) {
-        kafkaTemplate.send(TIPO_HABITACION_CREATED_TOPIC, event.getCodigo(), event);
+    public TipoHabitacionEventProducer(KafkaTemplate<String, Object> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
     }
 
-    public void publishUpdated(TipoHabitacionUpdatedEvent event) {
-        kafkaTemplate.send(TIPO_HABITACION_UPDATED_TOPIC, event.getCodigo(), event);
+    public void publishCreated(String key, Object event) {
+        kafkaTemplate.send(TIPO_HABITACION_CREATED_TOPIC, key, event);
     }
 
-    public void publishDeleted(TipoHabitacionDeletedEvent event) {
-        kafkaTemplate.send(TIPO_HABITACION_DELETED_TOPIC, event.getCodigo(), event);
+    public void publishUpdated(String key, Object event) {
+        kafkaTemplate.send(TIPO_HABITACION_UPDATED_TOPIC, key, event);
+    }
+
+    public void publishDeleted(String key, Object event) {
+        kafkaTemplate.send(TIPO_HABITACION_DELETED_TOPIC, key, event);
     }
 }
