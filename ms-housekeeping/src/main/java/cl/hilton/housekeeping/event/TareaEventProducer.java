abbfc70f28b053a2fe92588a21e6422789cefe21
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-28 16:52:00 -0400

    Remove common dependency from Tarea event producer

--- ms-housekeeping/src/main/java/cl/hilton/housekeeping/event/TareaEventProducer.java
+++ ms-housekeeping/src/main/java/cl/hilton/housekeeping/event/TareaEventProducer.java
@@ -1,14 +1,9 @@
 package cl.hilton.housekeeping.event;
 
-import cl.hilton.common.event.TareaCreatedEvent;
-import cl.hilton.common.event.TareaDeletedEvent;
-import cl.hilton.common.event.TareaUpdatedEvent;
-import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class TareaEventProducer {
 
     public static final String TAREA_CREATED_TOPIC = "tarea.created";
@@ -17,15 +12,19 @@ public class TareaEventProducer {
 
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
-    public void publishCreated(TareaCreatedEvent event) {
-        kafkaTemplate.send(TAREA_CREATED_TOPIC, event.getCodigo(), event);
+    public TareaEventProducer(KafkaTemplate<String, Object> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
     }
 
-    public void publishUpdated(TareaUpdatedEvent event) {
-        kafkaTemplate.send(TAREA_UPDATED_TOPIC, event.getCodigo(), event);
+    public void publishCreated(String key, Object event) {
+        kafkaTemplate.send(TAREA_CREATED_TOPIC, key, event);
     }
 
-    public void publishDeleted(TareaDeletedEvent event) {
-        kafkaTemplate.send(TAREA_DELETED_TOPIC, event.getCodigo(), event);
+    public void publishUpdated(String key, Object event) {
+        kafkaTemplate.send(TAREA_UPDATED_TOPIC, key, event);
+    }
+
+    public void publishDeleted(String key, Object event) {
+        kafkaTemplate.send(TAREA_DELETED_TOPIC, key, event);
     }
 }
