622a53934c3a4133bf05e75b27c8f2c5b4fa1d0e
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-28 15:14:11 -0400

    Enable Feign clients in tarifas

--- ms-tarifas/src/main/java/cl/hilton/tarifas/HotelTarifasApplication.java
+++ ms-tarifas/src/main/java/cl/hilton/tarifas/HotelTarifasApplication.java
@@ -2,8 +2,10 @@ package cl.hilton.tarifas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class HotelTarifasApplication {
 
 	public static void main(String[] args) {
