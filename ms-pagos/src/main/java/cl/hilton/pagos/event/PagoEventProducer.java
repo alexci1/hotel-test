3f80fe61ea646d83aeac354c49c6a8c3942190db
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-28 16:43:07 -0400

    Remove common dependency from Pago event producer

--- ms-pagos/src/main/java/cl/hilton/pagos/event/PagoEventProducer.java
+++ ms-pagos/src/main/java/cl/hilton/pagos/event/PagoEventProducer.java
@@ -1,14 +1,9 @@
 package cl.hilton.pagos.event;
 
-import cl.hilton.common.event.PagoCreatedEvent;
-import cl.hilton.common.event.PagoDeletedEvent;
-import cl.hilton.common.event.PagoUpdatedEvent;
-import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class PagoEventProducer {
 
     public static final String PAGO_CREATED_TOPIC = "pago.created";
@@ -17,15 +12,19 @@ public class PagoEventProducer {
 
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
-    public void publishCreated(PagoCreatedEvent event) {
-        kafkaTemplate.send(PAGO_CREATED_TOPIC, event.getNumeroFactura(), event);
+    public PagoEventProducer(KafkaTemplate<String, Object> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
     }
 
-    public void publishUpdated(PagoUpdatedEvent event) {
-        kafkaTemplate.send(PAGO_UPDATED_TOPIC, event.getNumeroFactura(), event);
+    public void publishCreated(String key, Object event) {
+        kafkaTemplate.send(PAGO_CREATED_TOPIC, key, event);
     }
 
-    public void publishDeleted(PagoDeletedEvent event) {
-        kafkaTemplate.send(PAGO_DELETED_TOPIC, event.getNumeroFactura(), event);
+    public void publishUpdated(String key, Object event) {
+        kafkaTemplate.send(PAGO_UPDATED_TOPIC, key, event);
+    }
+
+    public void publishDeleted(String key, Object event) {
+        kafkaTemplate.send(PAGO_DELETED_TOPIC, key, event);
     }
 }
