994fbb9399b6c552f7682568093b9867431caa17
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-28 16:46:35 -0400

    Remove common dependency from Movimiento event producer

--- ms-inventario/src/main/java/cl/hilton/inventario/event/MovimientoEventProducer.java
+++ ms-inventario/src/main/java/cl/hilton/inventario/event/MovimientoEventProducer.java
@@ -1,14 +1,9 @@
 package cl.hilton.inventario.event;
 
-import cl.hilton.common.event.MovimientoCreatedEvent;
-import cl.hilton.common.event.MovimientoDeletedEvent;
-import cl.hilton.common.event.MovimientoUpdatedEvent;
-import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class MovimientoEventProducer {
 
     public static final String MOVIMIENTO_CREATED_TOPIC = "movimiento.created";
@@ -17,15 +12,19 @@ public class MovimientoEventProducer {
 
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
-    public void publishCreated(MovimientoCreatedEvent event) {
-        kafkaTemplate.send(MOVIMIENTO_CREATED_TOPIC, event.getCodigoProducto(), event);
+    public MovimientoEventProducer(KafkaTemplate<String, Object> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
     }
 
-    public void publishUpdated(MovimientoUpdatedEvent event) {
-        kafkaTemplate.send(MOVIMIENTO_UPDATED_TOPIC, event.getCodigoProducto(), event);
+    public void publishCreated(String key, Object event) {
+        kafkaTemplate.send(MOVIMIENTO_CREATED_TOPIC, key, event);
     }
 
-    public void publishDeleted(MovimientoDeletedEvent event) {
-        kafkaTemplate.send(MOVIMIENTO_DELETED_TOPIC, event.getCodigoProducto(), event);
+    public void publishUpdated(String key, Object event) {
+        kafkaTemplate.send(MOVIMIENTO_UPDATED_TOPIC, key, event);
+    }
+
+    public void publishDeleted(String key, Object event) {
+        kafkaTemplate.send(MOVIMIENTO_DELETED_TOPIC, key, event);
     }
 }
