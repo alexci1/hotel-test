b13c7d7389c6c84466ba33f643d7d93f9ac1b748
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-28 16:31:33 -0400

    Remove common dependency from Checkout event producer

--- ms-checkin/src/main/java/cl/hilton/checkin/event/CheckoutEventProducer.java
+++ ms-checkin/src/main/java/cl/hilton/checkin/event/CheckoutEventProducer.java
@@ -1,14 +1,9 @@
 package cl.hilton.checkin.event;
 
-import cl.hilton.common.event.CheckoutCreatedEvent;
-import cl.hilton.common.event.CheckoutDeletedEvent;
-import cl.hilton.common.event.CheckoutUpdatedEvent;
-import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class CheckoutEventProducer {
 
     public static final String CHECKOUT_CREATED_TOPIC = "checkout.created";
@@ -17,15 +12,19 @@ public class CheckoutEventProducer {
 
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
-    public void publishCreated(CheckoutCreatedEvent event) {
-        kafkaTemplate.send(CHECKOUT_CREATED_TOPIC, event.getCodigoReserva(), event);
+    public CheckoutEventProducer(KafkaTemplate<String, Object> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
     }
 
-    public void publishUpdated(CheckoutUpdatedEvent event) {
-        kafkaTemplate.send(CHECKOUT_UPDATED_TOPIC, event.getCodigoReserva(), event);
+    public void publishCreated(String key, Object event) {
+        kafkaTemplate.send(CHECKOUT_CREATED_TOPIC, key, event);
     }
 
-    public void publishDeleted(CheckoutDeletedEvent event) {
-        kafkaTemplate.send(CHECKOUT_DELETED_TOPIC, event.getCodigoReserva(), event);
+    public void publishUpdated(String key, Object event) {
+        kafkaTemplate.send(CHECKOUT_UPDATED_TOPIC, key, event);
+    }
+
+    public void publishDeleted(String key, Object event) {
+        kafkaTemplate.send(CHECKOUT_DELETED_TOPIC, key, event);
     }
 }
