2080d558362f53711b61b5522f1a9162adc5cf2d
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-28 14:55:07 -0400

    Add Huesped Feign client to pagos

---
+++ ms-pagos/src/main/java/cl/hilton/pagos/client/HuespedClient.java
@@ -0,0 +1,14 @@
+package cl.hilton.pagos.client;
+
+import java.util.Map;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "ms-huespedes")
+public interface HuespedClient {
+
+    @GetMapping("/huespedes/email/{email}")
+    Map<String, Object> buscarPorEmail(@PathVariable("email") String email);
+}
