d05e5a37b25be09ee2d1122fa784e32c58f65f82
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-28 16:02:09 -0400

    Update notificaciones HuespedClient

--- ms-notificaciones/src/main/java/cl/hilton/notificaciones/client/HuespedClient.java
+++ ms-notificaciones/src/main/java/cl/hilton/notificaciones/client/HuespedClient.java
@@ -1,5 +1,6 @@
 package cl.hilton.notificaciones.client;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.cloud.openfeign.FeignClient;
@@ -8,6 +9,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "ms-huespedes")
 public interface HuespedClient {
+
+    @GetMapping("/huespedes")
+    List<Map<String, Object>> listar();
+
+    @GetMapping("/huespedes/{id}")
+    Map<String, Object> buscarPorId(@PathVariable("id") Long id);
 
     @GetMapping("/huespedes/email/{email}")
     Map<String, Object> buscarPorEmail(@PathVariable("email") String email);
