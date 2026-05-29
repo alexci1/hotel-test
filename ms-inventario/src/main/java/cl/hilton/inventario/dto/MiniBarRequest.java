92ffb62f722ad6061444e157888ac6b5a496eedd
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-29 04:16:05 -0400

    Update inventario MiniBarRequest DTO

--- ms-inventario/src/main/java/cl/hilton/inventario/dto/MiniBarRequest.java
+++ ms-inventario/src/main/java/cl/hilton/inventario/dto/MiniBarRequest.java
@@ -1,28 +1,26 @@
 package cl.hilton.inventario.dto;
 
-import jakarta.validation.constraints.*;
-import lombok.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+@Data
 public class MiniBarRequest {
 
-    @NotBlank
-    @Size(max = 10)
+    @NotBlank(message = "El numero de habitacion es obligatorio")
+    @Size(max = 10, message = "El numero de habitacion no puede superar los 10 caracteres")
     private String numeroHabitacion;
 
-    @NotBlank
-    @Size(max = 30)
+    @NotBlank(message = "El codigo del producto es obligatorio")
+    @Size(max = 30, message = "El codigo del producto no puede superar los 30 caracteres")
     private String codigoProducto;
 
-    @NotNull
-    @Min(0)
+    @Min(value = 0, message = "La cantidad no puede ser negativa")
     private Integer cantidad;
 
-    @NotNull
-    @Min(0)
+    @NotNull(message = "El precio unitario es obligatorio")
+    @Min(value = 0, message = "El precio unitario no puede ser negativo")
     private Integer precioUnitUsd;
 }
