5cec3152380b807e059fa6d569936b4a5dd5c5f9
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-29 00:49:45 -0400

    Update DescuentoRepository queries

--- ms-tarifas/src/main/java/cl/hilton/tarifas/repository/DescuentoRepository.java
+++ ms-tarifas/src/main/java/cl/hilton/tarifas/repository/DescuentoRepository.java
@@ -1,11 +1,13 @@
 package cl.hilton.tarifas.repository;
 
-import cl.hilton.tarifas.model.Descuento;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
+import cl.hilton.tarifas.model.Descuento;
 
 @Repository
 public interface DescuentoRepository extends JpaRepository<Descuento, Long> {
@@ -14,6 +16,11 @@ public interface DescuentoRepository extends JpaRepository<Descuento, Long> {
 
     boolean existsByCodigoDescuento(String codigoDescuento);
 
+    List<Descuento> findByAplicaA(String aplicaA);
+
     List<Descuento> findByActivo(Boolean activo);
 
+    List<Descuento> findByValidoDesde(LocalDate validoDesde);
+
+    List<Descuento> findByValidoHasta(LocalDate validoHasta);
 }
