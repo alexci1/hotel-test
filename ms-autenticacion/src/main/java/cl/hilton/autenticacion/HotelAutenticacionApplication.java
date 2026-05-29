4d3a8d13f5f795e897bf3af8d7c9b42e72d378c2
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-29 06:46:50 -0400

    Feign client agregada
    Se agregan feign client

--- ms-autenticacion/src/main/java/cl/hilton/autenticacion/HotelAutenticacionApplication.java
+++ ms-autenticacion/src/main/java/cl/hilton/autenticacion/HotelAutenticacionApplication.java
@@ -2,10 +2,12 @@ package cl.hilton.autenticacion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages ="cl.hilton.autenticacion")
 @SpringBootApplication
-@EnableFeignClients
 public class HotelAutenticacionApplication {
 
 	public static void main(String[] args) {
