1d05cd5a3fea491b803bf7da6b7487a4d998e63f
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-29 04:27:48 -0400

    Convert MiniBarMapper to MapStruct

--- ms-inventario/src/main/java/cl/hilton/inventario/mapper/MiniBarMapper.java
+++ ms-inventario/src/main/java/cl/hilton/inventario/mapper/MiniBarMapper.java
@@ -1,39 +1,32 @@
 package cl.hilton.inventario.mapper;
 
+import java.util.List;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
 import cl.hilton.inventario.dto.MiniBarRequest;
 import cl.hilton.inventario.dto.MiniBarResponse;
 import cl.hilton.inventario.model.MiniBar;
-import cl.hilton.inventario.model.Producto;
-import cl.hilton.inventario.model.ProjHabitacion;
-import org.springframework.stereotype.Component;
 
-@Component
-public class MiniBarMapper {
+@Mapper(componentModel = "spring")
+public interface MiniBarMapper {
 
-    public MiniBar toEntity(MiniBarRequest request, ProjHabitacion habitacion, Producto producto) {
-        return MiniBar.builder()
-                .habitacion(habitacion)
-                .producto(producto)
-                .cantidad(request.getCantidad())
-                .precioUnitUsd(request.getPrecioUnitUsd())
-                .build();
-    }
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "habitacion", ignore = true)
+    @Mapping(target = "producto", ignore = true)
+    MiniBar toEntity(MiniBarRequest request);
 
-    public MiniBarResponse toResponse(MiniBar miniBar) {
-        return MiniBarResponse.builder()
-                .id(miniBar.getId())
-                .numeroHabitacion(miniBar.getHabitacion().getNumeroHabitacion())
-                .codigoProducto(miniBar.getProducto().getCodigoProducto())
-                .nombreProducto(miniBar.getProducto().getNombre())
-                .cantidad(miniBar.getCantidad())
-                .precioUnitUsd(miniBar.getPrecioUnitUsd())
-                .build();
-    }
+    @Mapping(target = "numeroHabitacion", source = "habitacion.numeroHabitacion")
+    @Mapping(target = "codigoProducto", source = "producto.codigoProducto")
+    @Mapping(target = "nombreProducto", source = "producto.nombre")
+    MiniBarResponse toResponse(MiniBar miniBar);
 
-    public void updateEntity(MiniBar miniBar, MiniBarRequest request, ProjHabitacion habitacion, Producto producto) {
-        miniBar.setHabitacion(habitacion);
-        miniBar.setProducto(producto);
-        miniBar.setCantidad(request.getCantidad());
-        miniBar.setPrecioUnitUsd(request.getPrecioUnitUsd());
-    }
+    List<MiniBarResponse> toResponseList(List<MiniBar> minibares);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "habitacion", ignore = true)
+    @Mapping(target = "producto", ignore = true)
+    void updateEntity(MiniBarRequest request, @MappingTarget MiniBar miniBar);
 }
