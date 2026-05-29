207d0866e9f25081a1cb4f6633b41c75f5713305
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-28 21:10:15 -0400

    Agregar DTO request de proyección reserva en checkin

---
+++ ms-checkin/src/main/java/cl/hilton/checkin/dto/ProjReservaRequest.java
@@ -0,0 +1,37 @@
+package cl.hilton.checkin.dto;
+
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProjReservaRequest {
+
+    @NotBlank
+    @Size(max = 20)
+    private String codigoReserva;
+
+    @NotBlank
+    @Email
+    @Size(max = 120)
+    private String emailHuesped;
+
+    @NotBlank
+    @Size(max = 10)
+    private String numeroHabitacion;
+
+    @NotNull
+    private LocalDate fechaEntrada;
+
+    @NotNull
+    private LocalDate fechaSalida;
+
+    @NotBlank
+    @Size(max = 20)
+    private String estado;
+}
