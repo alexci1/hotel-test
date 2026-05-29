2ef7a9e8b5fc03effc858a43134001408fa219a5
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-29 06:56:41 -0400

    Update application

--- ms-pagos/src/main/java/cl/hilton/pagos/HotelPagosApplication.java
+++ ms-pagos/src/main/java/cl/hilton/pagos/HotelPagosApplication.java
@@ -2,14 +2,15 @@ package cl.hilton.pagos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages ="cl.hilton.pagos")
 @SpringBootApplication
-@EnableFeignClients
 public class HotelPagosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HotelPagosApplication.class, args);
 	}
-
 }
