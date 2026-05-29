6e9687c5d8e48b5a7f5d78ee4017f36c813c128c
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-28 21:51:14 -0400

    Fix ReporteRepository id type

--- ms-reportes/src/main/java/cl/hilton/reportes/repository/ReporteRepository.java
+++ ms-reportes/src/main/java/cl/hilton/reportes/repository/ReporteRepository.java
@@ -1,15 +1,15 @@
 package cl.hilton.reportes.repository;
 
-
-import cl.hilton.reportes.model.Reporte;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import cl.hilton.reportes.model.Reporte;
+
 @Repository
-public interface ReporteRepository extends JpaRepository<Reporte, Integer> {
+public interface ReporteRepository extends JpaRepository<Reporte, Long> {
 
     Optional<Reporte> findByCodigo(String codigo);
 
