d0a3ceb803b9641d7a3a517ddf696e190a4406cd
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-29 06:51:54 -0400

    Update reservas application

--- ms-reservas/src/main/java/cl/hilton/reservas/HotelReservasApplication.java
+++ ms-reservas/src/main/java/cl/hilton/reservas/HotelReservasApplication.java
@@ -2,14 +2,15 @@ package cl.hilton.reservas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages ="cl.hilton.reservas")
 @SpringBootApplication
-@EnableFeignClients
 public class HotelReservasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HotelReservasApplication.class, args);
 	}
-
 }
