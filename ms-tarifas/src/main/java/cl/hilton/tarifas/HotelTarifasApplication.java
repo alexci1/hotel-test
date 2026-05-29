8117345ad8a03ccb9540db7088c8a8ae0d9a72a2
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-29 06:58:38 -0400

    Update service

--- ms-tarifas/src/main/java/cl/hilton/tarifas/HotelTarifasApplication.java
+++ ms-tarifas/src/main/java/cl/hilton/tarifas/HotelTarifasApplication.java
@@ -2,14 +2,15 @@ package cl.hilton.tarifas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages ="cl.hilton.tarifas")
 @SpringBootApplication
-@EnableFeignClients
 public class HotelTarifasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HotelTarifasApplication.class, args);
 	}
-
 }
