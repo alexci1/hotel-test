c5a378f7b3f37356d35c935112109c4b714a9bda
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-28 21:09:11 -0400

    Agregar DTO request de proyección huésped en checkin

---
+++ ms-checkin/src/main/java/cl/hilton/checkin/dto/ProjHuespedRequest.java
@@ -0,0 +1,21 @@
+package cl.hilton.checkin.dto;
+
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProjHuespedRequest {
+
+    @NotBlank
+    @Email
+    @Size(max = 120)
+    private String email;
+
+    @NotBlank
+    @Size(max = 100)
+    private String nombreCompleto;
+}
