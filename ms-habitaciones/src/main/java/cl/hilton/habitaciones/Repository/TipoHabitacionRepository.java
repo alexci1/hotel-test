8031c2ba0db95f2cf56cf8ea80c6fda450cb3f77
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-29 01:43:55 -0400

    Update TipoHabitacionRepository queries

--- ms-habitaciones/src/main/java/cl/hilton/habitaciones/Repository/TipoHabitacionRepository.java
+++ ms-habitaciones/src/main/java/cl/hilton/habitaciones/Repository/TipoHabitacionRepository.java
@@ -1,11 +1,12 @@
 package cl.hilton.habitaciones.repository;
 
-import cl.hilton.habitaciones.model.TipoHabitacion;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
+import cl.hilton.habitaciones.model.TipoHabitacion;
 
 @Repository
 public interface TipoHabitacionRepository extends JpaRepository<TipoHabitacion, Long> {
@@ -17,4 +18,8 @@ public interface TipoHabitacionRepository extends JpaRepository<TipoHabitacion, 
     List<TipoHabitacion> findByActivo(Boolean activo);
 
     List<TipoHabitacion> findByCapacidadMax(Integer capacidadMax);
+
+    List<TipoHabitacion> findByCapacidadMaxGreaterThanEqual(Integer capacidadMax);
+
+    List<TipoHabitacion> findByDescripcionContainingIgnoreCase(String descripcion);
 }
