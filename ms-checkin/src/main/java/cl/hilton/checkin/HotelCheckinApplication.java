7afe989d101d08a8b84c7e964cc14efc8fe20c93
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-29 06:56:51 -0400

    Update application

--- ms-checkin/src/main/java/cl/hilton/checkin/HotelCheckinApplication.java
+++ ms-checkin/src/main/java/cl/hilton/checkin/HotelCheckinApplication.java
@@ -2,14 +2,15 @@ package cl.hilton.checkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages ="cl.hilton.checkin")
 @SpringBootApplication
-@EnableFeignClients
 public class HotelCheckinApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HotelCheckinApplication.class, args);
 	}
-
 }
