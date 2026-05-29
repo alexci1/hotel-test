66c8aa38c0e46b6b5520283dbf27eb2c3548b618
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-29 03:25:39 -0400

    Require email on PreferenciaRequest

--- ms-huespedes/src/main/java/cl/hilton/huespedes/dto/PreferenciaRequest.java
+++ ms-huespedes/src/main/java/cl/hilton/huespedes/dto/PreferenciaRequest.java
@@ -1,6 +1,7 @@
 package cl.hilton.huespedes.dto;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
@@ -8,6 +9,7 @@ import lombok.Data;
 @Data
 public class PreferenciaRequest {
 
+    @NotBlank(message = "El email del huesped es obligatorio")
     @Email(message = "El email del huesped debe tener formato valido")
     @Size(max = 120, message = "El email del huesped no puede superar los 120 caracteres")
     private String emailHuesped;
