c8cb0a22d1aeed08a40751b076a0f8075cc40a3c
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-28 22:30:28 -0400

    Add ProjHuespedMapper for reservas

---
+++ ms-reservas/src/main/java/cl/hilton/reservas/mapper/ProjHuespedMapper.java
@@ -0,0 +1,28 @@
+package cl.hilton.reservas.mapper;
+
+import java.util.List;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+import cl.hilton.reservas.dto.ProjHuespedRequest;
+import cl.hilton.reservas.dto.ProjHuespedResponse;
+import cl.hilton.reservas.model.ProjHuesped;
+
+@Mapper(componentModel = "spring")
+public interface ProjHuespedMapper {
+
+    @Mapping(target = "actualizadoEn", ignore = true)
+    @Mapping(target = "reservas", ignore = true)
+    ProjHuesped toEntity(ProjHuespedRequest request);
+
+    ProjHuespedResponse toResponse(ProjHuesped huesped);
+
+    List<ProjHuespedResponse> toResponseList(List<ProjHuesped> huespedes);
+
+    @Mapping(target = "email", ignore = true)
+    @Mapping(target = "actualizadoEn", ignore = true)
+    @Mapping(target = "reservas", ignore = true)
+    void updateEntity(ProjHuespedRequest request, @MappingTarget ProjHuesped huesped);
+}
