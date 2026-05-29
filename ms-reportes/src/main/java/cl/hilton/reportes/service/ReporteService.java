dec7d78a60f0c79a329dbe5f3747f3f70973dc27
lukasriquelme <luka.riquelmem@duocuc.cl>
2026-05-29 04:28:14 -0400

    Suprimir advertencias null en ReporteService

--- ms-reportes/src/main/java/cl/hilton/reportes/service/ReporteService.java
+++ ms-reportes/src/main/java/cl/hilton/reportes/service/ReporteService.java
@@ -14,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
+@SuppressWarnings("null")
 public class ReporteService {
 
     private final ReporteRepository reporteRepository;
