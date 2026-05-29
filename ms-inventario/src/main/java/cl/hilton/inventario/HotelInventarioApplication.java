c9e397858d6592620f729f8d9d8b3c2a38275e0d
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-29 07:05:13 -0400

    fix

--- ms-inventario/src/main/java/cl/hilton/inventario/HotelInventarioApplication.java
+++ ms-inventario/src/main/java/cl/hilton/inventario/HotelInventarioApplication.java
@@ -2,14 +2,15 @@ package cl.hilton.inventario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages ="cl.hilton.inventario.client")
 @SpringBootApplication
-@EnableFeignClients
 public class HotelInventarioApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HotelInventarioApplication.class, args);
 	}
-
 }
