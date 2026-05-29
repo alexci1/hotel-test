72670df43c6dcb98f0d710f09b08bd7a8c334139
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-28 22:13:06 -0400

    Update DisponibilidadRepository queries

--- ms-reservas/src/main/java/cl/hilton/reservas/repository/DisponibilidadRepository.java
+++ ms-reservas/src/main/java/cl/hilton/reservas/repository/DisponibilidadRepository.java
@@ -1,16 +1,26 @@
 package cl.hilton.reservas.repository;
 
-import cl.hilton.reservas.model.Disponibilidad;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
-import java.util.List;
+import cl.hilton.reservas.model.Disponibilidad;
 
 @Repository
 public interface DisponibilidadRepository extends JpaRepository<Disponibilidad, Long> {
 
+    Optional<Disponibilidad> findByHabitacionNumeroHabitacionAndFecha(String numeroHabitacion, LocalDate fecha);
+
+    boolean existsByHabitacionNumeroHabitacionAndFecha(String numeroHabitacion, LocalDate fecha);
+
+    List<Disponibilidad> findByHabitacionNumeroHabitacion(String numeroHabitacion);
+
     List<Disponibilidad> findByFecha(LocalDate fecha);
+
+    List<Disponibilidad> findByFechaBetween(LocalDate desde, LocalDate hasta);
 
     List<Disponibilidad> findByDisponible(Boolean disponible);
 }
