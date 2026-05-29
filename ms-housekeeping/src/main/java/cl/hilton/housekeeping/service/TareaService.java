8911876e72379ef4d405dbffffb6f00c9079690b
lukasriquelme <luka.riquelmem@duocuc.cl>
2026-05-29 03:29:51 -0400

    Suprimir advertencias null en TareaService

--- ms-housekeeping/src/main/java/cl/hilton/housekeeping/service/TareaService.java
+++ ms-housekeeping/src/main/java/cl/hilton/housekeeping/service/TareaService.java
@@ -14,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
+@SuppressWarnings("null")
 public class TareaService {
 
     private final TareaRepository tareaRepository;
