947e654d45d1a05c7611d2dc583efa8ad8a1bb4f
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-28 21:53:09 -0400

    Update MetricaRequest DTO

--- ms-reportes/src/main/java/cl/hilton/reportes/dto/MetricaRequest.java
+++ ms-reportes/src/main/java/cl/hilton/reportes/dto/MetricaRequest.java
@@ -1,33 +1,29 @@
 package cl.hilton.reportes.dto;
 
-import jakarta.validation.constraints.*;
-import lombok.*;
-
 import java.time.LocalDate;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
 public class MetricaRequest {
 
-    @NotBlank
-    @Size(max = 50)
+    @NotBlank(message = "El codigo del reporte es obligatorio")
+    @Size(max = 50, message = "El codigo del reporte no puede superar los 50 caracteres")
     private String codigoReporte;
 
-    @NotNull
+    @NotNull(message = "El periodo es obligatorio")
     private LocalDate periodo;
 
-    @NotBlank
-    @Size(max = 80)
+    @NotBlank(message = "El nombre de la metrica es obligatorio")
+    @Size(max = 80, message = "El nombre de la metrica no puede superar los 80 caracteres")
     private String nombreMetrica;
 
-    @NotNull
+    @NotNull(message = "El valor es obligatorio")
     private Integer valor;
 
-    @Size(max = 30)
+    @Size(max = 30, message = "La unidad no puede superar los 30 caracteres")
     private String unidad;
-
-    private LocalDate calculadoEn;
 }
