366508b1ddb8950093fe55722c23676d39e5f073
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-29 07:05:24 -0400

    fix

--- ms-notificaciones/src/main/java/cl/hilton/notificaciones/HotelNotificacionesApplication.java
+++ ms-notificaciones/src/main/java/cl/hilton/notificaciones/HotelNotificacionesApplication.java
@@ -2,14 +2,15 @@ package cl.hilton.notificaciones;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages ="cl.hilton.notificaciones.client")
 @SpringBootApplication
-@EnableFeignClients
 public class HotelNotificacionesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HotelNotificacionesApplication.class, args);
 	}
-
 }
