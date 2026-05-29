27f02307141d2e22e81423cdd92b942c5f0bdcec
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-29 01:20:15 -0400

    Update TemporadaController routes and DTOs

--- ms-tarifas/src/main/java/cl/hilton/tarifas/controller/TemporadaController.java
+++ ms-tarifas/src/main/java/cl/hilton/tarifas/controller/TemporadaController.java
@@ -1,37 +1,82 @@
 package cl.hilton.tarifas.controller;
 
-import cl.hilton.tarifas.model.Temporada;
-import cl.hilton.tarifas.service.TemporadaService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
+import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import cl.hilton.tarifas.dto.TemporadaRequest;
+import cl.hilton.tarifas.dto.TemporadaResponse;
+import cl.hilton.tarifas.service.TemporadaService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/temporadas")
+@RequestMapping("/api/v1/tarifas/temporadas")
 @RequiredArgsConstructor
 public class TemporadaController {
 
     private final TemporadaService temporadaService;
 
     @GetMapping
-    public List<Temporada> obtenerTemporadas() {
-        return temporadaService.obtenerTemporadas();
+    public List<TemporadaResponse> findAll() {
+        return temporadaService.findAll();
     }
 
     @GetMapping("/{id}")
-    public Optional<Temporada> obtenerPorId(@PathVariable Long id) {
-        return temporadaService.obtenerPorId(id);
+    public TemporadaResponse findById(@PathVariable Long id) {
+        return temporadaService.findById(id);
+    }
+
+    @GetMapping("/codigo/{codigo}")
+    public TemporadaResponse findByCodigo(@PathVariable String codigo) {
+        return temporadaService.findByCodigo(codigo);
+    }
+
+    @GetMapping("/buscar")
+    public List<TemporadaResponse> findByNombre(@RequestParam String nombre) {
+        return temporadaService.findByNombre(nombre);
+    }
+
+    @GetMapping("/inicio-antes-de/{fechaInicio}")
+    public List<TemporadaResponse> findByFechaInicioBefore(
+            @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate fechaInicio) {
+        return temporadaService.findByFechaInicioBefore(fechaInicio);
+    }
+
+    @GetMapping("/fin-despues-de/{fechaFin}")
+    public List<TemporadaResponse> findByFechaFinAfter(
+            @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate fechaFin) {
+        return temporadaService.findByFechaFinAfter(fechaFin);
     }
 
     @PostMapping
-    public Temporada guardarTemporada(@RequestBody Temporada temporada) {
-        return temporadaService.guardarTemporada(temporada);
+    @ResponseStatus(HttpStatus.CREATED)
+    public TemporadaResponse create(@Valid @RequestBody TemporadaRequest request) {
+        return temporadaService.create(request);
+    }
+
+    @PutMapping("/{id}")
+    public TemporadaResponse update(
+            @PathVariable Long id,
+            @Valid @RequestBody TemporadaRequest request) {
+        return temporadaService.update(id, request);
     }
 
     @DeleteMapping("/{id}")
-    public void eliminarTemporada(@PathVariable Long id) {
-        temporadaService.eliminarTemporada(id);
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long id) {
+        temporadaService.deleteById(id);
     }
 }
