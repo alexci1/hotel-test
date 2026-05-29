8653005be7def73f2051231c6a7f02495892c242
lukasriquelme <luka.riquelmem@duocuc.cl>
2026-05-29 02:53:55 -0400

    Ajustar DTO request de tareas housekeeping

--- ms-housekeeping/src/main/java/cl/hilton/housekeeping/dto/TareaRequest.java
+++ ms-housekeeping/src/main/java/cl/hilton/housekeeping/dto/TareaRequest.java
@@ -1,7 +1,14 @@
 package cl.hilton.housekeeping.dto;
 
-import jakarta.validation.constraints.*;
-import lombok.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
@@ -19,8 +26,7 @@ public class TareaRequest {
 
     @NotNull
     @Min(1)
-    private Long duracionMin;
+    private Integer duracionMin;
 
-    @NotNull
     private Boolean activa;
 }
