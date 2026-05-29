5b3d9c113a46b1fcb393dc0c2026961528489e2b
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-29 04:29:58 -0400

    Convert ProjHabitacionMapper to MapStruct

--- ms-inventario/src/main/java/cl/hilton/inventario/mapper/ProjHabitacionMapper.java
+++ ms-inventario/src/main/java/cl/hilton/inventario/mapper/ProjHabitacionMapper.java
@@ -1,32 +1,28 @@
 package cl.hilton.inventario.mapper;
 
+import java.util.List;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
 import cl.hilton.inventario.dto.ProjHabitacionRequest;
 import cl.hilton.inventario.dto.ProjHabitacionResponse;
 import cl.hilton.inventario.model.ProjHabitacion;
-import org.springframework.stereotype.Component;
 
-@Component
-public class ProjHabitacionMapper {
+@Mapper(componentModel = "spring")
+public interface ProjHabitacionMapper {
 
-    public ProjHabitacion toEntity(ProjHabitacionRequest request) {
-        return ProjHabitacion.builder()
-                .numeroHabitacion(request.getNumeroHabitacion())
-                .tipo(request.getTipo())
-                .actualizadoEn(request.getActualizadoEn())
-                .build();
-    }
+    @Mapping(target = "actualizadoEn", ignore = true)
+    @Mapping(target = "minibares", ignore = true)
+    ProjHabitacion toEntity(ProjHabitacionRequest request);
 
-    public ProjHabitacionResponse toResponse(ProjHabitacion habitacion) {
-        return ProjHabitacionResponse.builder()
-                .numeroHabitacion(habitacion.getNumeroHabitacion())
-                .tipo(habitacion.getTipo())
-                .actualizadoEn(habitacion.getActualizadoEn())
-                .build();
-    }
+    ProjHabitacionResponse toResponse(ProjHabitacion habitacion);
 
-    public void updateEntity(ProjHabitacion habitacion, ProjHabitacionRequest request) {
-        habitacion.setTipo(request.getTipo());
-        habitacion.setActualizadoEn(request.getActualizadoEn());
-    }
+    List<ProjHabitacionResponse> toResponseList(List<ProjHabitacion> habitaciones);
+
+    @Mapping(target = "numeroHabitacion", ignore = true)
+    @Mapping(target = "actualizadoEn", ignore = true)
+    @Mapping(target = "minibares", ignore = true)
+    void updateEntity(ProjHabitacionRequest request, @MappingTarget ProjHabitacion habitacion);
 }
-
