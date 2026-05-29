98feb5a5b7aeb3516664afacad146d2b4af82a49
lukasriquelme <luka.riquelmem@duocuc.cl>
2026-05-29 03:25:05 -0400

    Tipar HabitacionClient housekeeping

--- ms-housekeeping/src/main/java/cl/hilton/housekeeping/client/HabitacionClient.java
+++ ms-housekeeping/src/main/java/cl/hilton/housekeeping/client/HabitacionClient.java
@@ -1,14 +1,17 @@
 package cl.hilton.housekeeping.client;
 
-import java.util.Map;
-
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import cl.hilton.housekeeping.dto.ProjHabitacionResponse;
+
 @FeignClient(name = "ms-habitaciones")
 public interface HabitacionClient {
 
-    @GetMapping("/habitaciones/{id}")
-    Map<String, Object> buscarPorId(@PathVariable("id") Long id);
+    @GetMapping("/api/v1/habitaciones/{id}")
+    ProjHabitacionResponse buscarPorId(@PathVariable("id") Long id);
+
+    @GetMapping("/api/v1/habitaciones/numero/{numeroHabitacion}")
+    ProjHabitacionResponse buscarPorNumeroHabitacion(@PathVariable("numeroHabitacion") String numeroHabitacion);
 }
