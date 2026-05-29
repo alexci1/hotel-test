38368b19622c192fe4b8b5aa342b5736f7c70e9b
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-29 02:47:09 -0400

    Update DocumentoRepository queries

--- ms-huespedes/src/main/java/cl/hilton/huespedes/repository/DocumentoRepository.java
+++ ms-huespedes/src/main/java/cl/hilton/huespedes/repository/DocumentoRepository.java
@@ -1,10 +1,13 @@
 package cl.hilton.huespedes.repository;
 
-import cl.hilton.huespedes.model.Documento;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import cl.hilton.huespedes.model.Documento;
 
 @Repository
 public interface DocumentoRepository extends JpaRepository<Documento, Long> {
@@ -14,6 +17,10 @@ public interface DocumentoRepository extends JpaRepository<Documento, Long> {
     List<Documento> findByTipo(String tipo);
 
     List<Documento> findByPaisEmisor(String paisEmisor);
+
+    List<Documento> findByVencimiento(LocalDate vencimiento);
+
+    Optional<Documento> findByTipoAndNumeroAndPaisEmisor(String tipo, String numero, String paisEmisor);
 
     boolean existsByTipoAndNumeroAndPaisEmisor(String tipo, String numero, String paisEmisor);
 }
