8ce5cee0a3070db1857dfd029a755db59e5cfa88
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-29 01:47:16 -0400

    Update EstadoHabitacionRepository queries

--- ms-habitaciones/src/main/java/cl/hilton/habitaciones/Repository/EstadoHabitacionRepository.java
+++ ms-habitaciones/src/main/java/cl/hilton/habitaciones/Repository/EstadoHabitacionRepository.java
@@ -1,11 +1,13 @@
 package cl.hilton.habitaciones.repository;
 
-import cl.hilton.habitaciones.model.EstadoHabitacion;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
+import cl.hilton.habitaciones.model.EstadoHabitacion;
 
 @Repository
 public interface EstadoHabitacionRepository extends JpaRepository<EstadoHabitacion, Long> {
@@ -15,4 +17,6 @@ public interface EstadoHabitacionRepository extends JpaRepository<EstadoHabitaci
     boolean existsByHabitacionNumeroHabitacion(String numeroHabitacion);
 
     List<EstadoHabitacion> findByEstado(String estado);
+
+    List<EstadoHabitacion> findByActualizadoEn(LocalDate actualizadoEn);
 }
