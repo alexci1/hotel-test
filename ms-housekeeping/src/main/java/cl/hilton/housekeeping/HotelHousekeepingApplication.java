72cb80f7a7e63d3b38bc5bea96ae81a96407780d
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-29 07:04:58 -0400

    fix

--- ms-housekeeping/src/main/java/cl/hilton/housekeeping/HotelHousekeepingApplication.java
+++ ms-housekeeping/src/main/java/cl/hilton/housekeeping/HotelHousekeepingApplication.java
@@ -2,10 +2,12 @@ package cl.hilton.housekeeping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages ="cl.hilton.housekeeping.client")
 @SpringBootApplication
-@EnableFeignClients
 public class HotelHousekeepingApplication {
 
 	public static void main(String[] args) {
