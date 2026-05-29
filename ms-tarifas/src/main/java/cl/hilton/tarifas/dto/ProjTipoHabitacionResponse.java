ec32c2f83af3adf3504ba878636d42558b5446cf
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-29 01:09:01 -0400

    Fix ProjTipoHabitacionResponse fields

--- ms-tarifas/src/main/java/cl/hilton/tarifas/dto/ProjTipoHabitacionResponse.java
+++ ms-tarifas/src/main/java/cl/hilton/tarifas/dto/ProjTipoHabitacionResponse.java
@@ -1,15 +1,14 @@
 package cl.hilton.tarifas.dto;
 
+import java.time.LocalDate;
+
 import lombok.Data;
 
 @Data
 public class ProjTipoHabitacionResponse {
 
     private String codigo;
-
     private String descripcion;
-
     private Integer capacidadMax;
-
-    private Boolean activo;
+    private LocalDate actualizadoEn;
 }
