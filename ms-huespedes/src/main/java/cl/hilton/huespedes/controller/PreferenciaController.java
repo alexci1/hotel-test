cc274cb1bb4f5a41d29b80cba4011951f11b7d87
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-29 03:40:25 -0400

    Add empty PreferenciaController shell

---
+++ ms-huespedes/src/main/java/cl/hilton/huespedes/controller/PreferenciaController.java
@@ -0,0 +1,12 @@
+package cl.hilton.huespedes.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/huespedes/preferencias")
+@RequiredArgsConstructor
+public class PreferenciaController {
+}
