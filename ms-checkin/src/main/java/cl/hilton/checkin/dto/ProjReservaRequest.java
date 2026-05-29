c05003d3341a47a6fd68cf41023b36cd950cbe65
lukasriquelme <luka.riquelmem@duocuc.cl>
2026-05-29 02:10:23 -0400

    Ajustar DTO proyeccion reserva request

--- ms-checkin/src/main/java/cl/hilton/checkin/dto/ProjReservaRequest.java
+++ ms-checkin/src/main/java/cl/hilton/checkin/dto/ProjReservaRequest.java
@@ -1,9 +1,16 @@
 package cl.hilton.checkin.dto;
 
-import jakarta.validation.constraints.*;
-import lombok.*;
-
 import java.time.LocalDate;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
@@ -34,6 +41,4 @@ public class ProjReservaRequest {
     @NotBlank
     @Size(max = 20)
     private String estado;
-
-    private LocalDate actualizadoEn;
 }
