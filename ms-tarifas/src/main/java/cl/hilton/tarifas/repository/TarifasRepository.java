7893110ac249bd7128d0769cb648c2267348f0ef
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-29 00:48:35 -0400

    Update TarifasRepository queries

--- ms-tarifas/src/main/java/cl/hilton/tarifas/repository/TarifasRepository.java
+++ ms-tarifas/src/main/java/cl/hilton/tarifas/repository/TarifasRepository.java
@@ -1,16 +1,27 @@
 package cl.hilton.tarifas.repository;
 
-import cl.hilton.tarifas.model.Tarifa;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import cl.hilton.tarifas.model.Tarifa;
 
 @Repository
 public interface TarifasRepository extends JpaRepository<Tarifa, Long> {
+
+    Optional<Tarifa> findByTemporadaCodigoAndTipoHabitacionCodigo(String codigoTemporada, String tipoHabitacion);
+
+    boolean existsByTemporadaCodigoAndTipoHabitacionCodigo(String codigoTemporada, String tipoHabitacion);
+
+    List<Tarifa> findByTemporadaCodigo(String codigoTemporada);
+
+    List<Tarifa> findByTipoHabitacionCodigo(String tipoHabitacion);
 
     List<Tarifa> findByActiva(Boolean activa);
 
     List<Tarifa> findByIncluyeDesayuno(Boolean incluyeDesayuno);
 
+    List<Tarifa> findByTipoHabitacionCodigoAndActiva(String tipoHabitacion, Boolean activa);
 }
