9aeede93bde9d29035af6887f7abfa8c42ada551
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-28 22:19:32 -0400

    Update DisponibilidadRequest DTO

--- ms-reservas/src/main/java/cl/hilton/reservas/dto/DisponibilidadRequest.java
+++ ms-reservas/src/main/java/cl/hilton/reservas/dto/DisponibilidadRequest.java
@@ -1,19 +1,21 @@
 package cl.hilton.reservas.dto;
 
-import jakarta.validation.constraints.*;
-import lombok.Data;
-
 import java.time.LocalDate;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
 
 @Data
 public class DisponibilidadRequest {
 
-    @NotBlank(message = "El número de habitación es obligatorio")
+    @NotBlank(message = "El numero de habitacion es obligatorio")
+    @Size(max = 10, message = "El numero de habitacion no puede superar los 10 caracteres")
     private String numeroHabitacion;
 
     @NotNull(message = "La fecha es obligatoria")
     private LocalDate fecha;
 
-    @NotNull(message = "Debe indicar disponibilidad")
     private Boolean disponible;
 }
