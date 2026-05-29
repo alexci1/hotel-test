6f9a0b6bbe616fafea66d2c08c05a2699bb15217
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-29 06:50:35 -0400

    Alinear EnableFeignClients en ms-housekeeping

--- ms-housekeeping/src/main/java/cl/hilton/housekeeping/HotelHousekeepingApplication.java
+++ ms-housekeeping/src/main/java/cl/hilton/housekeeping/HotelHousekeepingApplication.java
@@ -2,12 +2,13 @@ package cl.hilton.housekeeping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@org.springframework.cloud.openfeign.EnableFeignClients
+@EnableFeignClients
 public class HotelHousekeepingApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(HotelHousekeepingApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(HotelHousekeepingApplication.class, args);
+	}
 }
