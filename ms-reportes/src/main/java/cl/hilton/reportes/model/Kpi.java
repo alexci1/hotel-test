ddf8bba72557acb8f70fc53a6df3774e739b5647
lukasriquelme <luka.riquelmem@duocuc.cl>
2026-05-29 04:03:45 -0400

    Agregar relacion de kpis con reportes

--- ms-reportes/src/main/java/cl/hilton/reportes/model/Kpi.java
+++ ms-reportes/src/main/java/cl/hilton/reportes/model/Kpi.java
@@ -4,9 +4,12 @@ import java.time.LocalDate;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
@@ -33,6 +36,10 @@ public class Kpi {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "codigo_reporte", referencedColumnName = "codigo", nullable = false)
+    private Reporte reporte;
 
     @Column(name = "nombre", nullable = false, length = 80, unique = true)
     private String nombre;
