a4754ec587d49f63c772d30073a8b24c5c2ac7d5
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-29 06:58:03 -0400

    Update service

--- ms-reportes/src/main/java/cl/hilton/reportes/HotelReportesApplication.java
+++ ms-reportes/src/main/java/cl/hilton/reportes/HotelReportesApplication.java
@@ -2,14 +2,15 @@ package cl.hilton.reportes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages ="cl.hilton.reportes")
 @SpringBootApplication
-@EnableFeignClients
 public class HotelReportesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HotelReportesApplication.class, args);
 	}
-
 }
