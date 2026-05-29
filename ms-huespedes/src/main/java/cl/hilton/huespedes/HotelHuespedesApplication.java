d78c0e752ba82c7157493e45f50b25e1bd66b1d9
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-05-29 06:55:08 -0400

    Update huespedes

--- ms-huespedes/src/main/java/cl/hilton/huespedes/HotelHuespedesApplication.java
+++ ms-huespedes/src/main/java/cl/hilton/huespedes/HotelHuespedesApplication.java
@@ -2,10 +2,12 @@ package cl.hilton.huespedes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages ="cl.hilton.huespedes")
 @SpringBootApplication
-@EnableFeignClients
 public class HotelHuespedesApplication {
 
 	public static void main(String[] args) {
