c46e5c39133abd039508eb80278e1bf442b7162f
lukasriquelme <luka.riquelmem@duocuc.cl>
2026-06-03 19:41:08 -0400

    Fix minibar repository class name

--- ms-inventario/src/main/java/cl/hilton/inventario/repository/MinibarRepository.java
+++ ms-inventario/src/main/java/cl/hilton/inventario/repository/MinibarRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface MiniBarRepository extends JpaRepository<MiniBar, Long> {
+public interface MinibarRepository extends JpaRepository<MiniBar, Long> {
 
     List<MiniBar> findByHabitacionNumeroHabitacion(String numeroHabitacion);
 
