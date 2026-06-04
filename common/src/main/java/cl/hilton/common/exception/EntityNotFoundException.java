d1b0658c38393ac7ccd3bb4ca592c83038f16b0f
lukasriquelme <luka.riquelmem@duocuc.cl>
2026-06-03 21:42:15 -0400

    Fix common entity not found exception package

--- common/src/main/java/cl/hilton/common/exception/EntityNotFoundException.java
+++ common/src/main/java/cl/hilton/common/exception/EntityNotFoundException.java
@@ -1,9 +1,13 @@
-package cl.triskeledu.common.exception;
+package cl.hilton.common.exception;
 
 public class EntityNotFoundException extends RuntimeException {
 
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+
     public EntityNotFoundException(String entity, String key, Object value) {
-        super(String.format("No se encontraron registros en %s con el %s '%s'", 
+        super(String.format("No se encontraron registros en %s con el %s '%s'",
             entity, key, (value != null ? value.toString() : "N/A")));
     }
 }
