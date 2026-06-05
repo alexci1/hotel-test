c11861f079ef6b24f444818d77f10cc594daf628
lukasriquelme <luka.riquelmem@duocuc.cl>
2026-06-04 21:31:23 -0400

    Use common entity not found exception in key service

--- ms-checkin/src/main/java/cl/hilton/checkin/service/LlaveService.java
+++ ms-checkin/src/main/java/cl/hilton/checkin/service/LlaveService.java
@@ -13,7 +13,7 @@ import cl.hilton.checkin.model.Llave;
 import cl.hilton.checkin.model.ProjReserva;
 import cl.hilton.checkin.repository.LlaveRepository;
 import cl.hilton.checkin.repository.ProjReservaRepository;
-import jakarta.persistence.EntityNotFoundException;
+import cl.hilton.common.exception.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 
 @Service
