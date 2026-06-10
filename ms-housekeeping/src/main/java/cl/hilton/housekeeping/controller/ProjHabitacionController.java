5c7903ed38eef75bdf368f51f9c877d4710516ed
lukasriquelme <luka.riquelmem@duocuc.cl>
2026-06-10 00:03:57 -0400

    Update ProjHabitacionController.java

--- ms-housekeeping/src/main/java/cl/hilton/housekeeping/controller/ProjHabitacionController.java
+++ ms-housekeeping/src/main/java/cl/hilton/housekeeping/controller/ProjHabitacionController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/proj-habitaciones")
+@RequestMapping("/api/v1/habitaciones-proyeccion")
 @RequiredArgsConstructor
 public class ProjHabitacionController {
 
