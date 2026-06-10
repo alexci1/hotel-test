61aa817eb6b83bcae2436ff89325eaed24d75d12
Alexander Ccarita <al.ccarita@duocuc.cl>
2026-06-09 22:31:35 -0400

    Se modifica end point de controller projHabitacion
    Se procede a hacer cambio en el consumer

--- ms-housekeeping/src/main/java/cl/hilton/housekeeping/controller/ProjHabitacionController.java
+++ ms-housekeeping/src/main/java/cl/hilton/housekeeping/controller/ProjHabitacionController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/housekeeping/proj-habitaciones")
+@RequestMapping("/api/v1/proj-habitaciones")
 @RequiredArgsConstructor
 public class ProjHabitacionController {
 
