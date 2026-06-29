68251f4834be3815342c225cdf0a5ee6479742ef
constanza <co.barrag@duocuc.cl>
2026-06-29 18:26:32 -0400

    Add HATEOAS links to movimiento controller

--- ms-inventario/src/main/java/cl/hilton/inventario/controller/MovimientoController.java
+++ ms-inventario/src/main/java/cl/hilton/inventario/controller/MovimientoController.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,6 +23,9 @@ import cl.hilton.inventario.service.MovimientoService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @RestController
 @RequestMapping("/api/v1/movimientos")
 @RequiredArgsConstructor
@@ -29,63 +33,87 @@ public class MovimientoController {
 
     private final MovimientoService movimientoService;
 
+    private MovimientoResponse addLinks(MovimientoResponse m) {
+        m.add(linkTo(methodOn(MovimientoController.class).findById(m.getId())).withSelfRel());
+        m.add(linkTo(methodOn(MovimientoController.class).update(m.getId(), null)).withRel("update"));
+        m.add(linkTo(methodOn(MovimientoController.class).deleteById(m.getId())).withRel("delete"));
+        m.add(linkTo(methodOn(MovimientoController.class).findAll()).withRel("all"));
+        return m;
+    }
+
     @GetMapping
-    public List<MovimientoResponse> findAll() {
-        return movimientoService.findAll();
+    public CollectionModel<MovimientoResponse> findAll() {
+        List<MovimientoResponse> list = movimientoService.findAll();
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(MovimientoController.class).findAll()).withSelfRel());
     }
 
     @GetMapping("/{id}")
     public MovimientoResponse findById(@PathVariable Long id) {
-        return movimientoService.findById(id);
+        return addLinks(movimientoService.findById(id));
     }
 
     @GetMapping("/producto/{codigoProducto}")
-    public List<MovimientoResponse> findByCodigoProducto(@PathVariable String codigoProducto) {
-        return movimientoService.findByCodigoProducto(codigoProducto);
+    public CollectionModel<MovimientoResponse> findByCodigoProducto(@PathVariable String codigoProducto) {
+        List<MovimientoResponse> list = movimientoService.findByCodigoProducto(codigoProducto);
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(MovimientoController.class).findByCodigoProducto(codigoProducto)).withSelfRel());
     }
 
     @GetMapping("/tipo/{tipo}")
-    public List<MovimientoResponse> findByTipo(@PathVariable String tipo) {
-        return movimientoService.findByTipo(tipo);
+    public CollectionModel<MovimientoResponse> findByTipo(@PathVariable String tipo) {
+        List<MovimientoResponse> list = movimientoService.findByTipo(tipo);
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(MovimientoController.class).findByTipo(tipo)).withSelfRel());
     }
 
     @GetMapping("/registrado-por/{registradoPor}")
-    public List<MovimientoResponse> findByRegistradoPor(@PathVariable String registradoPor) {
-        return movimientoService.findByRegistradoPor(registradoPor);
+    public CollectionModel<MovimientoResponse> findByRegistradoPor(@PathVariable String registradoPor) {
+        List<MovimientoResponse> list = movimientoService.findByRegistradoPor(registradoPor);
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(MovimientoController.class).findByRegistradoPor(registradoPor)).withSelfRel());
     }
 
     @GetMapping("/fecha/{registradoEn}")
-    public List<MovimientoResponse> findByRegistradoEn(
+    public CollectionModel<MovimientoResponse> findByRegistradoEn(
             @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate registradoEn) {
-        return movimientoService.findByRegistradoEn(registradoEn);
+        List<MovimientoResponse> list = movimientoService.findByRegistradoEn(registradoEn);
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(MovimientoController.class).findByRegistradoEn(registradoEn)).withSelfRel());
     }
 
     @GetMapping("/rango")
-    public List<MovimientoResponse> findByRangoFechas(
+    public CollectionModel<MovimientoResponse> findByRangoFechas(
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate desde,
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate hasta) {
-        return movimientoService.findByRangoFechas(desde, hasta);
+        List<MovimientoResponse> list = movimientoService.findByRangoFechas(desde, hasta);
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(MovimientoController.class).findByRangoFechas(desde, hasta)).withSelfRel());
     }
 
     @GetMapping("/cantidad-mayor/{cantidad}")
-    public List<MovimientoResponse> findByCantidadGreaterThan(@PathVariable Integer cantidad) {
-        return movimientoService.findByCantidadGreaterThan(cantidad);
+    public CollectionModel<MovimientoResponse> findByCantidadGreaterThan(@PathVariable Integer cantidad) {
+        List<MovimientoResponse> list = movimientoService.findByCantidadGreaterThan(cantidad);
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(MovimientoController.class).findByCantidadGreaterThan(cantidad)).withSelfRel());
     }
 
     @GetMapping("/cantidad-menor/{cantidad}")
-    public List<MovimientoResponse> findByCantidadLessThan(@PathVariable Integer cantidad) {
-        return movimientoService.findByCantidadLessThan(cantidad);
+    public CollectionModel<MovimientoResponse> findByCantidadLessThan(@PathVariable Integer cantidad) {
+        List<MovimientoResponse> list = movimientoService.findByCantidadLessThan(cantidad);
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(MovimientoController.class).findByCantidadLessThan(cantidad)).withSelfRel());
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public MovimientoResponse create(@Valid @RequestBody MovimientoRequest request) {
-        return movimientoService.create(request);
+        return addLinks(movimientoService.create(request));
     }
 
     @PutMapping("/{id}")
     public MovimientoResponse update(@PathVariable Long id, @Valid @RequestBody MovimientoRequest request) {
-        return movimientoService.update(id, request);
+        return addLinks(movimientoService.update(id, request));
     }
 
     @DeleteMapping("/{id}")
