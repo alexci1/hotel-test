f21950a64975ba29a5f825fd0cc38f76b6be6fb0
constanza <co.barrag@duocuc.cl>
2026-06-29 18:25:35 -0400

    Add HATEOAS links to cancelacion controller

--- ms-reservas/src/main/java/cl/hilton/reservas/controller/CancelacionController.java
+++ ms-reservas/src/main/java/cl/hilton/reservas/controller/CancelacionController.java
@@ -4,14 +4,17 @@ import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +24,9 @@ import cl.hilton.reservas.service.CancelacionService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @RestController
 @RequestMapping("/api/v1/cancelaciones")
 @RequiredArgsConstructor
@@ -28,38 +34,47 @@ public class CancelacionController {
 
     private final CancelacionService cancelacionService;
 
+    private CancelacionResponse addLinks(CancelacionResponse c) {
+        c.add(linkTo(methodOn(CancelacionController.class).findById(c.getId())).withSelfRel());
+        c.add(linkTo(methodOn(CancelacionController.class).update(c.getId(), null)).withRel("update").withTitle("PUT - Actualizar cancelacion"));
+        c.add(linkTo(methodOn(CancelacionController.class).deleteById(c.getId())).withRel("delete").withTitle("DELETE - Eliminar cancelacion"));
+        c.add(linkTo(methodOn(CancelacionController.class).findAll()).withRel("all").withTitle("GET - Todas las cancelaciones"));
+        return c;
+    }
+
     @GetMapping
-    public List<CancelacionResponse> findAll() {
-        return cancelacionService.findAll();
+    public CollectionModel<CancelacionResponse> findAll() {
+        List<CancelacionResponse> list = cancelacionService.findAll();
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(CancelacionController.class).findAll()).withSelfRel());
     }
 
     @GetMapping("/{id}")
     public CancelacionResponse findById(@PathVariable Long id) {
-        return cancelacionService.findById(id);
+        return addLinks(cancelacionService.findById(id));
     }
 
     @GetMapping("/reserva/{codigoReserva}")
     public CancelacionResponse findByCodigoReserva(@PathVariable String codigoReserva) {
-        return cancelacionService.findByCodigoReserva(codigoReserva);
+        return addLinks(cancelacionService.findByCodigoReserva(codigoReserva));
     }
 
     @GetMapping("/fecha/{canceladoEn}")
-    public List<CancelacionResponse> findByCanceladoEn(
-            @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate canceladoEn) {
-        return cancelacionService.findByCanceladoEn(canceladoEn);
+    public CollectionModel<CancelacionResponse> findByCanceladoEn(@PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate canceladoEn) {
+        List<CancelacionResponse> list = cancelacionService.findByCanceladoEn(canceladoEn);
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(CancelacionController.class).findByCanceladoEn(canceladoEn)).withSelfRel());
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public CancelacionResponse create(@Valid @RequestBody CancelacionRequest request) {
-        return cancelacionService.create(request);
+        return addLinks(cancelacionService.create(request));
     }
 
     @PutMapping("/{id}")
-    public CancelacionResponse update(
-            @PathVariable Long id,
-            @Valid @RequestBody CancelacionRequest request) {
-        return cancelacionService.update(id, request);
+    public CancelacionResponse update(@PathVariable Long id, @Valid @RequestBody CancelacionRequest request) {
+        return addLinks(cancelacionService.update(id, request));
     }
 
     @DeleteMapping("/{id}")
