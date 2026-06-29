ea36085d6c5dc32784827f3c9181a2e711a156ef
constanza <co.barrag@duocuc.cl>
2026-06-29 18:18:10 -0400

    Add HATEOAS links to cargo controller

--- ms-pagos/src/main/java/cl/hilton/pagos/controller/CargoController.java
+++ ms-pagos/src/main/java/cl/hilton/pagos/controller/CargoController.java
@@ -3,6 +3,7 @@ package cl.hilton.pagos.controller;
 import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +21,9 @@ import cl.hilton.pagos.service.CargoService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @RestController
 @RequestMapping("/api/v1/cargos")
 @RequiredArgsConstructor
@@ -27,42 +31,58 @@ public class CargoController {
 
     private final CargoService cargoService;
 
+    private CargoResponse addLinks(CargoResponse c) {
+        c.add(linkTo(methodOn(CargoController.class).findById(c.getId())).withSelfRel());
+        c.add(linkTo(methodOn(CargoController.class).update(c.getId(), null)).withRel("update").withTitle("PUT - Actualizar cargo"));
+        c.add(linkTo(methodOn(CargoController.class).deleteById(c.getId())).withRel("delete").withTitle("DELETE - Eliminar cargo"));
+        c.add(linkTo(methodOn(CargoController.class).findAll()).withRel("all").withTitle("GET - Todos los cargos"));
+        return c;
+    }
+
     @GetMapping
-    public List<CargoResponse> findAll() {
-        return cargoService.findAll();
+    public CollectionModel<CargoResponse> findAll() {
+        List<CargoResponse> list = cargoService.findAll();
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(CargoController.class).findAll()).withSelfRel());
     }
 
     @GetMapping("/{id}")
     public CargoResponse findById(@PathVariable Long id) {
-        return cargoService.findById(id);
+        return addLinks(cargoService.findById(id));
     }
 
     @GetMapping("/factura/{numeroFactura}")
-    public List<CargoResponse> findByNumeroFactura(@PathVariable String numeroFactura) {
-        return cargoService.findByNumeroFactura(numeroFactura);
+    public CollectionModel<CargoResponse> findByNumeroFactura(@PathVariable String numeroFactura) {
+        List<CargoResponse> list = cargoService.findByNumeroFactura(numeroFactura);
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(CargoController.class).findByNumeroFactura(numeroFactura)).withSelfRel());
     }
 
     @GetMapping("/origen/{origen}")
-    public List<CargoResponse> findByOrigen(@PathVariable String origen) {
-        return cargoService.findByOrigen(origen);
+    public CollectionModel<CargoResponse> findByOrigen(@PathVariable String origen) {
+        List<CargoResponse> list = cargoService.findByOrigen(origen);
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(CargoController.class).findByOrigen(origen)).withSelfRel());
     }
 
     @GetMapping("/fecha/{registradoEn}")
-    public List<CargoResponse> findByRegistradoEn(@PathVariable LocalDate registradoEn) {
-        return cargoService.findByRegistradoEn(registradoEn);
+    public CollectionModel<CargoResponse> findByRegistradoEn(@PathVariable LocalDate registradoEn) {
+        List<CargoResponse> list = cargoService.findByRegistradoEn(registradoEn);
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(CargoController.class).findByRegistradoEn(registradoEn)).withSelfRel());
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public CargoResponse create(@Valid @RequestBody CargoRequest request) {
-        return cargoService.create(request);
+        return addLinks(cargoService.create(request));
     }
 
     @PutMapping("/{id}")
     public CargoResponse update(
             @PathVariable Long id,
             @Valid @RequestBody CargoRequest request) {
-        return cargoService.update(id, request);
+        return addLinks(cargoService.update(id, request));
     }
 
     @DeleteMapping("/{id}")
