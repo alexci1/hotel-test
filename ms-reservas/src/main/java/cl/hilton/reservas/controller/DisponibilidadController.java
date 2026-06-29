d85d6946151a5fa3b0adeb33f7adb83ca62d7884
constanza <co.barrag@duocuc.cl>
2026-06-29 18:25:52 -0400

    Add HATEOAS links to disponibilidad controller

--- ms-reservas/src/main/java/cl/hilton/reservas/controller/DisponibilidadController.java
+++ ms-reservas/src/main/java/cl/hilton/reservas/controller/DisponibilidadController.java
@@ -4,9 +4,11 @@ import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -22,6 +24,9 @@ import cl.hilton.reservas.service.DisponibilidadService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @RestController
 @RequestMapping("/api/v1/disponibilidades")
 @RequiredArgsConstructor
@@ -29,57 +34,68 @@ public class DisponibilidadController {
 
     private final DisponibilidadService disponibilidadService;
 
+    private DisponibilidadResponse addLinks(DisponibilidadResponse d) {
+        d.add(linkTo(methodOn(DisponibilidadController.class).findById(d.getId())).withSelfRel());
+        d.add(linkTo(methodOn(DisponibilidadController.class).update(d.getId(), null)).withRel("update").withTitle("PUT - Actualizar disponibilidad"));
+        d.add(linkTo(methodOn(DisponibilidadController.class).deleteById(d.getId())).withRel("delete").withTitle("DELETE - Eliminar disponibilidad"));
+        d.add(linkTo(methodOn(DisponibilidadController.class).findAll()).withRel("all").withTitle("GET - Todas las disponibilidades"));
+        return d;
+    }
+
     @GetMapping
-    public List<DisponibilidadResponse> findAll() {
-        return disponibilidadService.findAll();
+    public CollectionModel<DisponibilidadResponse> findAll() {
+        List<DisponibilidadResponse> list = disponibilidadService.findAll();
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(DisponibilidadController.class).findAll()).withSelfRel());
     }
 
     @GetMapping("/{id}")
     public DisponibilidadResponse findById(@PathVariable Long id) {
-        return disponibilidadService.findById(id);
+        return addLinks(disponibilidadService.findById(id));
     }
 
     @GetMapping("/habitacion/{numeroHabitacion}/fecha/{fecha}")
-    public DisponibilidadResponse findByHabitacionAndFecha(
-            @PathVariable String numeroHabitacion,
-            @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate fecha) {
-        return disponibilidadService.findByHabitacionAndFecha(numeroHabitacion, fecha);
+    public DisponibilidadResponse findByHabitacionAndFecha(@PathVariable String numeroHabitacion, @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate fecha) {
+        return addLinks(disponibilidadService.findByHabitacionAndFecha(numeroHabitacion, fecha));
     }
 
     @GetMapping("/habitacion/{numeroHabitacion}")
-    public List<DisponibilidadResponse> findByNumeroHabitacion(@PathVariable String numeroHabitacion) {
-        return disponibilidadService.findByNumeroHabitacion(numeroHabitacion);
+    public CollectionModel<DisponibilidadResponse> findByNumeroHabitacion(@PathVariable String numeroHabitacion) {
+        List<DisponibilidadResponse> list = disponibilidadService.findByNumeroHabitacion(numeroHabitacion);
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(DisponibilidadController.class).findByNumeroHabitacion(numeroHabitacion)).withSelfRel());
     }
 
     @GetMapping("/fecha/{fecha}")
-    public List<DisponibilidadResponse> findByFecha(
-            @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate fecha) {
-        return disponibilidadService.findByFecha(fecha);
+    public CollectionModel<DisponibilidadResponse> findByFecha(@PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate fecha) {
+        List<DisponibilidadResponse> list = disponibilidadService.findByFecha(fecha);
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(DisponibilidadController.class).findByFecha(fecha)).withSelfRel());
     }
 
     @GetMapping("/rango")
-    public List<DisponibilidadResponse> findByRangoFechas(
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate desde,
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate hasta) {
-        return disponibilidadService.findByRangoFechas(desde, hasta);
+    public CollectionModel<DisponibilidadResponse> findByRangoFechas(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate desde, @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate hasta) {
+        List<DisponibilidadResponse> list = disponibilidadService.findByRangoFechas(desde, hasta);
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(DisponibilidadController.class).findByRangoFechas(desde, hasta)).withSelfRel());
     }
 
     @GetMapping("/disponible/{disponible}")
-    public List<DisponibilidadResponse> findByDisponible(@PathVariable Boolean disponible) {
-        return disponibilidadService.findByDisponible(disponible);
+    public CollectionModel<DisponibilidadResponse> findByDisponible(@PathVariable Boolean disponible) {
+        List<DisponibilidadResponse> list = disponibilidadService.findByDisponible(disponible);
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(DisponibilidadController.class).findByDisponible(disponible)).withSelfRel());
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public DisponibilidadResponse create(@Valid @RequestBody DisponibilidadRequest request) {
-        return disponibilidadService.create(request);
+        return addLinks(disponibilidadService.create(request));
     }
 
     @PutMapping("/{id}")
-    public DisponibilidadResponse update(
-            @PathVariable Long id,
-            @Valid @RequestBody DisponibilidadRequest request) {
-        return disponibilidadService.update(id, request);
+    public DisponibilidadResponse update(@PathVariable Long id, @Valid @RequestBody DisponibilidadRequest request) {
+        return addLinks(disponibilidadService.update(id, request));
     }
 
     @DeleteMapping("/{id}")
