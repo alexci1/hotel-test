08d4394ad3642f576d30805e02dfd9d559fdb589
constanza <co.barrag@duocuc.cl>
2026-06-29 18:28:22 -0400

    Add HATEOAS links to mesa controller

--- ms-restaurante/src/main/java/cl/hilton/restaurante/controller/MesaController.java
+++ ms-restaurante/src/main/java/cl/hilton/restaurante/controller/MesaController.java
@@ -2,6 +2,7 @@ package cl.hilton.restaurante.controller;
 
 import java.util.List;
 
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,6 +22,9 @@ import cl.hilton.restaurante.service.MesaService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @RestController
 @RequestMapping("/api/v1/mesas")
 @RequiredArgsConstructor
@@ -28,61 +32,80 @@ public class MesaController {
 
     private final MesaService mesaService;
 
+    private MesaResponse addLinks(MesaResponse m) {
+        m.add(linkTo(methodOn(MesaController.class).findById(m.getId())).withSelfRel());
+        m.add(linkTo(methodOn(MesaController.class).update(m.getId(), null)).withRel("update").withTitle("PUT - Actualizar mesa"));
+        m.add(linkTo(methodOn(MesaController.class).deleteById(m.getId())).withRel("delete").withTitle("DELETE - Eliminar mesa"));
+        m.add(linkTo(methodOn(MesaController.class).cambiarDisponibilidad(m.getId(), null)).withRel("cambiar-disponibilidad").withTitle("PATCH - Cambiar disponibilidad"));
+        m.add(linkTo(methodOn(MesaController.class).findAll()).withRel("all").withTitle("GET - Todas las mesas"));
+        return m;
+    }
+
     @GetMapping
-    public List<MesaResponse> findAll() {
-        return mesaService.findAll();
+    public CollectionModel<MesaResponse> findAll() {
+        List<MesaResponse> list = mesaService.findAll();
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(MesaController.class).findAll()).withSelfRel());
     }
 
     @GetMapping("/{id}")
     public MesaResponse findById(@PathVariable Long id) {
-        return mesaService.findById(id);
+        return addLinks(mesaService.findById(id));
     }
 
     @GetMapping("/numero/{numeroMesa}")
     public MesaResponse findByNumeroMesa(@PathVariable String numeroMesa) {
-        return mesaService.findByNumeroMesa(numeroMesa);
+        return addLinks(mesaService.findByNumeroMesa(numeroMesa));
     }
 
     @GetMapping("/zona/{zona}")
-    public List<MesaResponse> findByZona(@PathVariable String zona) {
-        return mesaService.findByZona(zona);
+    public CollectionModel<MesaResponse> findByZona(@PathVariable String zona) {
+        List<MesaResponse> list = mesaService.findByZona(zona);
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(MesaController.class).findByZona(zona)).withSelfRel());
     }
 
     @GetMapping("/disponible/{disponible}")
-    public List<MesaResponse> findByDisponible(@PathVariable Boolean disponible) {
-        return mesaService.findByDisponible(disponible);
+    public CollectionModel<MesaResponse> findByDisponible(@PathVariable Boolean disponible) {
+        List<MesaResponse> list = mesaService.findByDisponible(disponible);
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(MesaController.class).findByDisponible(disponible)).withSelfRel());
     }
 
     @GetMapping("/capacidad-minima/{capacidad}")
-    public List<MesaResponse> findByCapacidadMinima(@PathVariable Integer capacidad) {
-        return mesaService.findByCapacidadMinima(capacidad);
+    public CollectionModel<MesaResponse> findByCapacidadMinima(@PathVariable Integer capacidad) {
+        List<MesaResponse> list = mesaService.findByCapacidadMinima(capacidad);
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(MesaController.class).findByCapacidadMinima(capacidad)).withSelfRel());
     }
 
     @GetMapping("/zona/{zona}/disponible/{disponible}")
-    public List<MesaResponse> findByZonaAndDisponible(
+    public CollectionModel<MesaResponse> findByZonaAndDisponible(
             @PathVariable String zona,
             @PathVariable Boolean disponible) {
-        return mesaService.findByZonaAndDisponible(zona, disponible);
+        List<MesaResponse> list = mesaService.findByZonaAndDisponible(zona, disponible);
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(MesaController.class).findByZonaAndDisponible(zona, disponible)).withSelfRel());
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public MesaResponse create(@Valid @RequestBody MesaRequest request) {
-        return mesaService.create(request);
+        return addLinks(mesaService.create(request));
     }
 
     @PutMapping("/{id}")
     public MesaResponse update(
             @PathVariable Long id,
             @Valid @RequestBody MesaRequest request) {
-        return mesaService.update(id, request);
+        return addLinks(mesaService.update(id, request));
     }
 
     @PatchMapping("/{id}/disponibilidad")
     public MesaResponse cambiarDisponibilidad(
             @PathVariable Long id,
             @RequestParam Boolean disponible) {
-        return mesaService.cambiarDisponibilidad(id, disponible);
+        return addLinks(mesaService.cambiarDisponibilidad(id, disponible));
     }
 
     @DeleteMapping("/{id}")
