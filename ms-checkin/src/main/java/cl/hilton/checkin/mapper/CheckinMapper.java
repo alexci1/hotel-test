cb63f84bd89b50917b7684692531f25e336de2fc
constanza <co.barrag@duocuc.cl>
2026-06-29 19:47:41 -0400

    Ignore HATEOAS links in checkin mapper

--- ms-checkin/src/main/java/cl/hilton/checkin/mapper/CheckinMapper.java
+++ ms-checkin/src/main/java/cl/hilton/checkin/mapper/CheckinMapper.java
@@ -22,6 +22,7 @@ public interface CheckinMapper {
     @Mapping(target = "fechaHora", ignore = true)
     Checkin toEntity(CheckinRequest request, ProjReserva reserva, ProjHuesped huesped);
 
+    @Mapping(target = "links", ignore = true)
     @Mapping(target = "codigoReserva", source = "reserva.codigoReserva")
     @Mapping(target = "emailHuesped", source = "huesped.email")
     @Mapping(target = "nombreHuesped", source = "huesped.nombreCompleto")
