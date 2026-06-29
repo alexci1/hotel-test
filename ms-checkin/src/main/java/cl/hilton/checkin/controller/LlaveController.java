7b496cd9edf5f268562392dec64297cea6acedd6
constanza <co.barrag@duocuc.cl>
2026-06-29 18:17:08 -0400

    Add HATEOAS links to llave controller

--- ms-checkin/src/main/java/cl/hilton/checkin/controller/LlaveController.java
+++ ms-checkin/src/main/java/cl/hilton/checkin/controller/LlaveController.java
@@ -2,6 +2,7 @@ package cl.hilton.checkin.controller;
 
 import java.util.List;
 
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,6 +22,9 @@ import cl.hilton.checkin.service.LlaveService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @RestController
 @RequestMapping("/api/v1/llaves")
 @RequiredArgsConstructor
@@ -28,50 +32,67 @@ public class LlaveController {
 
     private final LlaveService llaveService;
 
+    private LlaveResponse addLinks(LlaveResponse l) {
+        l.add(linkTo(methodOn(LlaveController.class).findById(l.getId())).withSelfRel());
+        l.add(linkTo(methodOn(LlaveController.class).update(l.getId(), null)).withRel("update").withTitle("PUT - Actualizar llave"));
+        l.add(linkTo(methodOn(LlaveController.class).deleteById(l.getId())).withRel("delete").withTitle("DELETE - Eliminar llave"));
+        l.add(linkTo(methodOn(LlaveController.class).updateEstado(l.getId(), null)).withRel("cambiar-estado").withTitle("PATCH - Cambiar estado llave"));
+        l.add(linkTo(methodOn(LlaveController.class).findAll()).withRel("all").withTitle("GET - Todas las llaves"));
+        return l;
+    }
+
     @GetMapping
-    public List<LlaveResponse> findAll() {
-        return llaveService.findAll();
+    public CollectionModel<LlaveResponse> findAll() {
+        List<LlaveResponse> list = llaveService.findAll();
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(LlaveController.class).findAll()).withSelfRel());
     }
 
     @GetMapping("/{id}")
     public LlaveResponse findById(@PathVariable Long id) {
-        return llaveService.findById(id);
+        return addLinks(llaveService.findById(id));
     }
 
     @GetMapping("/codigo/{codigoLlave}")
     public LlaveResponse findByCodigoLlave(@PathVariable String codigoLlave) {
-        return llaveService.findByCodigoLlave(codigoLlave);
+        return addLinks(llaveService.findByCodigoLlave(codigoLlave));
     }
 
     @GetMapping("/habitacion/{numeroHabitacion}")
-    public List<LlaveResponse> findByNumeroHabitacion(@PathVariable String numeroHabitacion) {
-        return llaveService.findByNumeroHabitacion(numeroHabitacion);
+    public CollectionModel<LlaveResponse> findByNumeroHabitacion(@PathVariable String numeroHabitacion) {
+        List<LlaveResponse> list = llaveService.findByNumeroHabitacion(numeroHabitacion);
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(LlaveController.class).findByNumeroHabitacion(numeroHabitacion)).withSelfRel());
     }
 
     @GetMapping("/activa/{activa}")
-    public List<LlaveResponse> findByActiva(@PathVariable Boolean activa) {
-        return llaveService.findByActiva(activa);
+    public CollectionModel<LlaveResponse> findByActiva(@PathVariable Boolean activa) {
+        List<LlaveResponse> list = llaveService.findByActiva(activa);
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(LlaveController.class).findByActiva(activa)).withSelfRel());
     }
 
     @GetMapping("/reserva/{codigoReserva}")
-    public List<LlaveResponse> findByCodigoReserva(@PathVariable String codigoReserva) {
-        return llaveService.findByCodigoReserva(codigoReserva);
+    public CollectionModel<LlaveResponse> findByCodigoReserva(@PathVariable String codigoReserva) {
+        List<LlaveResponse> list = llaveService.findByCodigoReserva(codigoReserva);
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(LlaveController.class).findByCodigoReserva(codigoReserva)).withSelfRel());
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public LlaveResponse create(@Valid @RequestBody LlaveRequest request) {
-        return llaveService.create(request);
+        return addLinks(llaveService.create(request));
     }
 
     @PutMapping("/{id}")
     public LlaveResponse update(@PathVariable Long id, @Valid @RequestBody LlaveRequest request) {
-        return llaveService.update(id, request);
+        return addLinks(llaveService.update(id, request));
     }
 
     @PatchMapping("/{id}/estado")
     public LlaveResponse updateEstado(@PathVariable Long id, @RequestParam Boolean activa) {
-        return llaveService.updateEstado(id, activa);
+        return addLinks(llaveService.updateEstado(id, activa));
     }
 
     @DeleteMapping("/{id}")
