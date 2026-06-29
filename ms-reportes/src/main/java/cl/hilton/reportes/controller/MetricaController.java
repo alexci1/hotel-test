8e09ba3714853d3e7938fe9482aa6bd21ef62521
constanza <co.barrag@duocuc.cl>
2026-06-29 18:22:21 -0400

    Add HATEOAS links to metrica controller

--- ms-reportes/src/main/java/cl/hilton/reportes/controller/MetricaController.java
+++ ms-reportes/src/main/java/cl/hilton/reportes/controller/MetricaController.java
@@ -4,9 +4,11 @@ import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -22,6 +24,9 @@ import cl.hilton.reportes.service.MetricaService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @RestController
 @RequestMapping("/api/v1/metricas")
 @RequiredArgsConstructor
@@ -29,56 +34,70 @@ public class MetricaController {
 
     private final MetricaService metricaService;
 
+    private MetricaResponse addLinks(MetricaResponse m) {
+        m.add(linkTo(methodOn(MetricaController.class).findById(m.getId())).withSelfRel());
+        m.add(linkTo(methodOn(MetricaController.class).update(m.getId(), null)).withRel("update").withTitle("PUT - Actualizar métrica"));
+        m.add(linkTo(methodOn(MetricaController.class).deleteById(m.getId())).withRel("delete").withTitle("DELETE - Eliminar métrica"));
+        m.add(linkTo(methodOn(MetricaController.class).findAll()).withRel("all").withTitle("GET - Todas las métricas"));
+        return m;
+    }
+
     @GetMapping
-    public List<MetricaResponse> findAll() {
-        return metricaService.findAll();
+    public CollectionModel<MetricaResponse> findAll() {
+        List<MetricaResponse> list = metricaService.findAll();
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(MetricaController.class).findAll()).withSelfRel());
     }
 
     @GetMapping("/{id}")
     public MetricaResponse findById(@PathVariable Long id) {
-        return metricaService.findById(id);
+        return addLinks(metricaService.findById(id));
     }
 
     @GetMapping("/reporte/{codigoReporte}")
-    public List<MetricaResponse> findByReporte(@PathVariable String codigoReporte) {
-        return metricaService.findByReporte(codigoReporte);
+    public CollectionModel<MetricaResponse> findByReporte(@PathVariable String codigoReporte) {
+        List<MetricaResponse> list = metricaService.findByReporte(codigoReporte);
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(MetricaController.class).findByReporte(codigoReporte)).withSelfRel());
     }
 
     @GetMapping("/periodo/{periodo}")
-    public List<MetricaResponse> findByPeriodo(
-            @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate periodo) {
-        return metricaService.findByPeriodo(periodo);
+    public CollectionModel<MetricaResponse> findByPeriodo(@PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate periodo) {
+        List<MetricaResponse> list = metricaService.findByPeriodo(periodo);
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(MetricaController.class).findByPeriodo(periodo)).withSelfRel());
     }
 
     @GetMapping("/rango")
-    public List<MetricaResponse> findByRangoFechas(
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate desde,
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate hasta) {
-        return metricaService.findByRangoFechas(desde, hasta);
+    public CollectionModel<MetricaResponse> findByRangoFechas(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate desde, @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate hasta) {
+        List<MetricaResponse> list = metricaService.findByRangoFechas(desde, hasta);
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(MetricaController.class).findByRangoFechas(desde, hasta)).withSelfRel());
     }
 
     @GetMapping("/nombre/{nombreMetrica}")
-    public List<MetricaResponse> findByNombreMetrica(@PathVariable String nombreMetrica) {
-        return metricaService.findByNombreMetrica(nombreMetrica);
+    public CollectionModel<MetricaResponse> findByNombreMetrica(@PathVariable String nombreMetrica) {
+        List<MetricaResponse> list = metricaService.findByNombreMetrica(nombreMetrica);
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(MetricaController.class).findByNombreMetrica(nombreMetrica)).withSelfRel());
     }
 
     @GetMapping("/calculado/{calculadoEn}")
-    public List<MetricaResponse> findByCalculadoEn(
-            @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate calculadoEn) {
-        return metricaService.findByCalculadoEn(calculadoEn);
+    public CollectionModel<MetricaResponse> findByCalculadoEn(@PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate calculadoEn) {
+        List<MetricaResponse> list = metricaService.findByCalculadoEn(calculadoEn);
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(MetricaController.class).findByCalculadoEn(calculadoEn)).withSelfRel());
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public MetricaResponse create(@Valid @RequestBody MetricaRequest request) {
-        return metricaService.create(request);
+        return addLinks(metricaService.create(request));
     }
 
     @PutMapping("/{id}")
-    public MetricaResponse update(
-            @PathVariable Long id,
-            @Valid @RequestBody MetricaRequest request) {
-        return metricaService.update(id, request);
+    public MetricaResponse update(@PathVariable Long id, @Valid @RequestBody MetricaRequest request) {
+        return addLinks(metricaService.update(id, request));
     }
 
     @DeleteMapping("/{id}")
