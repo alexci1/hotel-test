5e04c6a4bca4dcdc28d6e9ecf2f6d259d4794570
constanza <co.barrag@duocuc.cl>
2026-06-29 18:19:12 -0400

    Add HATEOAS links to plantilla controller

--- ms-notificaciones/src/main/java/cl/hilton/notificaciones/controller/PlantillaController.java
+++ ms-notificaciones/src/main/java/cl/hilton/notificaciones/controller/PlantillaController.java
@@ -2,6 +2,7 @@ package cl.hilton.notificaciones.controller;
 
 import java.util.List;
 
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +20,9 @@ import cl.hilton.notificaciones.service.PlantillaService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @RestController
 @RequestMapping("/api/v1/plantillas")
 @RequiredArgsConstructor
@@ -26,42 +30,56 @@ public class PlantillaController {
 
     private final PlantillaService plantillaService;
 
+    private PlantillaResponse addLinks(PlantillaResponse p) {
+        p.add(linkTo(methodOn(PlantillaController.class).findById(p.getId())).withSelfRel());
+        p.add(linkTo(methodOn(PlantillaController.class).update(p.getId(), null)).withRel("update").withTitle("PUT - Actualizar plantilla"));
+        p.add(linkTo(methodOn(PlantillaController.class).deleteById(p.getId())).withRel("delete").withTitle("DELETE - Eliminar plantilla"));
+        p.add(linkTo(methodOn(PlantillaController.class).findAll()).withRel("all").withTitle("GET - Todas las plantillas"));
+        return p;
+    }
+
     @GetMapping
-    public List<PlantillaResponse> findAll() {
-        return plantillaService.findAll();
+    public CollectionModel<PlantillaResponse> findAll() {
+        List<PlantillaResponse> list = plantillaService.findAll();
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(PlantillaController.class).findAll()).withSelfRel());
     }
 
     @GetMapping("/{id}")
     public PlantillaResponse findById(@PathVariable Long id) {
-        return plantillaService.findById(id);
+        return addLinks(plantillaService.findById(id));
     }
 
     @GetMapping("/codigo/{codigo}")
     public PlantillaResponse findByCodigo(@PathVariable String codigo) {
-        return plantillaService.findByCodigo(codigo);
+        return addLinks(plantillaService.findByCodigo(codigo));
     }
 
     @GetMapping("/canal/{canal}")
-    public List<PlantillaResponse> findByCanal(@PathVariable String canal) {
-        return plantillaService.findByCanal(canal);
+    public CollectionModel<PlantillaResponse> findByCanal(@PathVariable String canal) {
+        List<PlantillaResponse> list = plantillaService.findByCanal(canal);
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(PlantillaController.class).findByCanal(canal)).withSelfRel());
     }
 
     @GetMapping("/activa/{activa}")
-    public List<PlantillaResponse> findByActiva(@PathVariable Boolean activa) {
-        return plantillaService.findByActiva(activa);
+    public CollectionModel<PlantillaResponse> findByActiva(@PathVariable Boolean activa) {
+        List<PlantillaResponse> list = plantillaService.findByActiva(activa);
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(PlantillaController.class).findByActiva(activa)).withSelfRel());
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public PlantillaResponse create(@Valid @RequestBody PlantillaRequest request) {
-        return plantillaService.create(request);
+        return addLinks(plantillaService.create(request));
     }
 
     @PutMapping("/{id}")
     public PlantillaResponse update(
             @PathVariable Long id,
             @Valid @RequestBody PlantillaRequest request) {
-        return plantillaService.update(id, request);
+        return addLinks(plantillaService.update(id, request));
     }
 
     @DeleteMapping("/{id}")
