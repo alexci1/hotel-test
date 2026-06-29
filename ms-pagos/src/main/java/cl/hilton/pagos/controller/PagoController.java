d3a1d8155a01f402db98743bbe32c499e9b9c956
constanza <co.barrag@duocuc.cl>
2026-06-29 18:17:56 -0400

    Add HATEOAS links to pago controller

--- ms-pagos/src/main/java/cl/hilton/pagos/controller/PagoController.java
+++ ms-pagos/src/main/java/cl/hilton/pagos/controller/PagoController.java
@@ -3,6 +3,7 @@ package cl.hilton.pagos.controller;
 import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +21,9 @@ import cl.hilton.pagos.service.PagoService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @RestController
 @RequestMapping("/api/v1/pagos")
 @RequiredArgsConstructor
@@ -27,42 +31,58 @@ public class PagoController {
 
     private final PagoService pagoService;
 
+    private PagoResponse addLinks(PagoResponse p) {
+        p.add(linkTo(methodOn(PagoController.class).findById(p.getId())).withSelfRel());
+        p.add(linkTo(methodOn(PagoController.class).update(p.getId(), null)).withRel("update").withTitle("PUT - Actualizar pago"));
+        p.add(linkTo(methodOn(PagoController.class).deleteById(p.getId())).withRel("delete").withTitle("DELETE - Eliminar pago"));
+        p.add(linkTo(methodOn(PagoController.class).findAll()).withRel("all").withTitle("GET - Todos los pagos"));
+        return p;
+    }
+
     @GetMapping
-    public List<PagoResponse> findAll() {
-        return pagoService.findAll();
+    public CollectionModel<PagoResponse> findAll() {
+        List<PagoResponse> list = pagoService.findAll();
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(PagoController.class).findAll()).withSelfRel());
     }
 
     @GetMapping("/{id}")
     public PagoResponse findById(@PathVariable Long id) {
-        return pagoService.findById(id);
+        return addLinks(pagoService.findById(id));
     }
 
     @GetMapping("/factura/{numeroFactura}")
-    public List<PagoResponse> findByNumeroFactura(@PathVariable String numeroFactura) {
-        return pagoService.findByNumeroFactura(numeroFactura);
+    public CollectionModel<PagoResponse> findByNumeroFactura(@PathVariable String numeroFactura) {
+        List<PagoResponse> list = pagoService.findByNumeroFactura(numeroFactura);
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(PagoController.class).findByNumeroFactura(numeroFactura)).withSelfRel());
     }
 
     @GetMapping("/metodo/{metodo}")
-    public List<PagoResponse> findByMetodo(@PathVariable String metodo) {
-        return pagoService.findByMetodo(metodo);
+    public CollectionModel<PagoResponse> findByMetodo(@PathVariable String metodo) {
+        List<PagoResponse> list = pagoService.findByMetodo(metodo);
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(PagoController.class).findByMetodo(metodo)).withSelfRel());
     }
 
     @GetMapping("/fecha/{pagadoEn}")
-    public List<PagoResponse> findByPagadoEn(@PathVariable LocalDate pagadoEn) {
-        return pagoService.findByPagadoEn(pagadoEn);
+    public CollectionModel<PagoResponse> findByPagadoEn(@PathVariable LocalDate pagadoEn) {
+        List<PagoResponse> list = pagoService.findByPagadoEn(pagadoEn);
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(PagoController.class).findByPagadoEn(pagadoEn)).withSelfRel());
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public PagoResponse create(@Valid @RequestBody PagoRequest request) {
-        return pagoService.create(request);
+        return addLinks(pagoService.create(request));
     }
 
     @PutMapping("/{id}")
     public PagoResponse update(
             @PathVariable Long id,
             @Valid @RequestBody PagoRequest request) {
-        return pagoService.update(id, request);
+        return addLinks(pagoService.update(id, request));
     }
 
     @DeleteMapping("/{id}")
