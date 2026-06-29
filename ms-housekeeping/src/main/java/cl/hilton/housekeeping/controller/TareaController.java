2c0b209941eac7d7303dac90d7f8dfcec8acdade
constanza <co.barrag@duocuc.cl>
2026-06-29 18:22:38 -0400

    Add HATEOAS links to tarea controller

--- ms-housekeeping/src/main/java/cl/hilton/housekeeping/controller/TareaController.java
+++ ms-housekeeping/src/main/java/cl/hilton/housekeeping/controller/TareaController.java
@@ -2,6 +2,7 @@ package cl.hilton.housekeeping.controller;
 
 import java.util.List;
 
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +21,9 @@ import cl.hilton.housekeeping.service.TareaService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @RestController
 @RequestMapping("/api/v1/tareas")
 @RequiredArgsConstructor
@@ -27,40 +31,54 @@ public class TareaController {
 
     private final TareaService tareaService;
 
+    private TareaResponse addLinks(TareaResponse t) {
+        t.add(linkTo(methodOn(TareaController.class).findById(t.getId())).withSelfRel());
+        t.add(linkTo(methodOn(TareaController.class).update(t.getId(), null)).withRel("update").withTitle("PUT - Actualizar tarea"));
+        t.add(linkTo(methodOn(TareaController.class).deleteById(t.getId())).withRel("delete").withTitle("DELETE - Eliminar tarea"));
+        t.add(linkTo(methodOn(TareaController.class).findAll()).withRel("all").withTitle("GET - Todas las tareas"));
+        return t;
+    }
+
     @GetMapping
-    public List<TareaResponse> findAll() {
-        return tareaService.findAll();
+    public CollectionModel<TareaResponse> findAll() {
+        List<TareaResponse> list = tareaService.findAll();
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(TareaController.class).findAll()).withSelfRel());
     }
 
     @GetMapping("/{id}")
     public TareaResponse findById(@PathVariable Long id) {
-        return tareaService.findById(id);
+        return addLinks(tareaService.findById(id));
     }
 
     @GetMapping("/codigo/{codigo}")
     public TareaResponse findByCodigo(@PathVariable String codigo) {
-        return tareaService.findByCodigo(codigo);
+        return addLinks(tareaService.findByCodigo(codigo));
     }
 
     @GetMapping("/activa/{activa}")
-    public List<TareaResponse> findByActiva(@PathVariable Boolean activa) {
-        return tareaService.findByActiva(activa);
+    public CollectionModel<TareaResponse> findByActiva(@PathVariable Boolean activa) {
+        List<TareaResponse> list = tareaService.findByActiva(activa);
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(TareaController.class).findByActiva(activa)).withSelfRel());
     }
 
     @GetMapping("/descripcion")
-    public List<TareaResponse> findByDescripcion(@RequestParam String descripcion) {
-        return tareaService.findByDescripcion(descripcion);
+    public CollectionModel<TareaResponse> findByDescripcion(@RequestParam String descripcion) {
+        List<TareaResponse> list = tareaService.findByDescripcion(descripcion);
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(TareaController.class).findByDescripcion(descripcion)).withSelfRel());
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public TareaResponse create(@Valid @RequestBody TareaRequest request) {
-        return tareaService.create(request);
+        return addLinks(tareaService.create(request));
     }
 
     @PutMapping("/{id}")
     public TareaResponse update(@PathVariable Long id, @Valid @RequestBody TareaRequest request) {
-        return tareaService.update(id, request);
+        return addLinks(tareaService.update(id, request));
     }
 
     @DeleteMapping("/{id}")
