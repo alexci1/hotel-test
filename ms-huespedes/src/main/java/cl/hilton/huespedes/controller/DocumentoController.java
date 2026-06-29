799f9c992a964be35eb4692fe15a050bec0d1560
constanza <co.barrag@duocuc.cl>
2026-06-29 18:05:21 -0400

    Add HATEOAS links to documento controller

--- ms-huespedes/src/main/java/cl/hilton/huespedes/controller/DocumentoController.java
+++ ms-huespedes/src/main/java/cl/hilton/huespedes/controller/DocumentoController.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,6 +22,9 @@ import cl.hilton.huespedes.service.DocumentoService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @RestController
 @RequestMapping("/api/v1/documentos")
 @RequiredArgsConstructor
@@ -28,35 +32,53 @@ public class DocumentoController {
 
     private final DocumentoService documentoService;
 
+    private DocumentoResponse addLinks(DocumentoResponse d) {
+        d.add(linkTo(methodOn(DocumentoController.class).findById(d.getId())).withSelfRel());
+        d.add(linkTo(methodOn(DocumentoController.class).update(d.getId(), null)).withRel("update").withTitle("PUT - Actualizar documento"));
+        d.add(linkTo(methodOn(DocumentoController.class).deleteById(d.getId())).withRel("delete").withTitle("DELETE - Eliminar documento"));
+        d.add(linkTo(methodOn(DocumentoController.class).findAll()).withRel("all").withTitle("GET - Todos los documentos"));
+        return d;
+    }
+
     @GetMapping
-    public List<DocumentoResponse> findAll() {
-        return documentoService.findAll();
+    public CollectionModel<DocumentoResponse> findAll() {
+        List<DocumentoResponse> list = documentoService.findAll();
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(DocumentoController.class).findAll()).withSelfRel());
     }
 
     @GetMapping("/{id}")
     public DocumentoResponse findById(@PathVariable Long id) {
-        return documentoService.findById(id);
+        return addLinks(documentoService.findById(id));
     }
 
     @GetMapping("/huesped/{emailHuesped}")
-    public List<DocumentoResponse> findByEmailHuesped(@PathVariable String emailHuesped) {
-        return documentoService.findByEmailHuesped(emailHuesped);
+    public CollectionModel<DocumentoResponse> findByEmailHuesped(@PathVariable String emailHuesped) {
+        List<DocumentoResponse> list = documentoService.findByEmailHuesped(emailHuesped);
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(DocumentoController.class).findByEmailHuesped(emailHuesped)).withSelfRel());
     }
 
     @GetMapping("/tipo/{tipo}")
-    public List<DocumentoResponse> findByTipo(@PathVariable String tipo) {
-        return documentoService.findByTipo(tipo);
+    public CollectionModel<DocumentoResponse> findByTipo(@PathVariable String tipo) {
+        List<DocumentoResponse> list = documentoService.findByTipo(tipo);
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(DocumentoController.class).findByTipo(tipo)).withSelfRel());
     }
 
     @GetMapping("/pais/{paisEmisor}")
-    public List<DocumentoResponse> findByPaisEmisor(@PathVariable String paisEmisor) {
-        return documentoService.findByPaisEmisor(paisEmisor);
+    public CollectionModel<DocumentoResponse> findByPaisEmisor(@PathVariable String paisEmisor) {
+        List<DocumentoResponse> list = documentoService.findByPaisEmisor(paisEmisor);
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(DocumentoController.class).findByPaisEmisor(paisEmisor)).withSelfRel());
     }
 
     @GetMapping("/vencimiento/{vencimiento}")
-    public List<DocumentoResponse> findByVencimiento(
+    public CollectionModel<DocumentoResponse> findByVencimiento(
             @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate vencimiento) {
-        return documentoService.findByVencimiento(vencimiento);
+        List<DocumentoResponse> list = documentoService.findByVencimiento(vencimiento);
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(DocumentoController.class).findByVencimiento(vencimiento)).withSelfRel());
     }
 
     @GetMapping("/tipo/{tipo}/numero/{numero}/pais/{paisEmisor}")
@@ -64,18 +86,18 @@ public class DocumentoController {
             @PathVariable String tipo,
             @PathVariable String numero,
             @PathVariable String paisEmisor) {
-        return documentoService.findByTipoNumeroPais(tipo, numero, paisEmisor);
+        return addLinks(documentoService.findByTipoNumeroPais(tipo, numero, paisEmisor));
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public DocumentoResponse create(@Valid @RequestBody DocumentoRequest request) {
-        return documentoService.create(request);
+        return addLinks(documentoService.create(request));
     }
 
     @PutMapping("/{id}")
     public DocumentoResponse update(@PathVariable Long id, @Valid @RequestBody DocumentoRequest request) {
-        return documentoService.update(id, request);
+        return addLinks(documentoService.update(id, request));
     }
 
     @DeleteMapping("/{id}")
