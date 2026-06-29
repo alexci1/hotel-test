017ce7acb91ada9d164418e27bc198040a6f79a1
constanza <co.barrag@duocuc.cl>
2026-06-29 17:55:51 -0400

    Add HATEOAS links to estado habitacion controller

--- ms-habitaciones/src/main/java/cl/hilton/habitaciones/controller/EstadoHabitacionController.java
+++ ms-habitaciones/src/main/java/cl/hilton/habitaciones/controller/EstadoHabitacionController.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +24,9 @@ import cl.hilton.habitaciones.service.EstadoHabitacionService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @RestController
 @RequestMapping("/api/v1/estados")
 @RequiredArgsConstructor
@@ -30,50 +34,64 @@ public class EstadoHabitacionController {
 
     private final EstadoHabitacionService estadoHabitacionService;
 
+    private EstadoHabitacionResponse addLinks(EstadoHabitacionResponse e) {
+        e.add(linkTo(methodOn(EstadoHabitacionController.class).findById(e.getId())).withSelfRel());
+        e.add(linkTo(methodOn(EstadoHabitacionController.class).update(e.getId(), null)).withRel("update").withTitle("PUT - Actualizar estado"));
+        e.add(linkTo(methodOn(EstadoHabitacionController.class).deleteById(e.getId())).withRel("delete").withTitle("DELETE - Eliminar estado"));
+        e.add(linkTo(methodOn(EstadoHabitacionController.class).findAll()).withRel("all").withTitle("GET - Todos los estados"));
+        return e;
+    }
+
     @GetMapping
-    public List<EstadoHabitacionResponse> findAll() {
-        return estadoHabitacionService.findAll();
+    public CollectionModel<EstadoHabitacionResponse> findAll() {
+        List<EstadoHabitacionResponse> list = estadoHabitacionService.findAll();
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(EstadoHabitacionController.class).findAll()).withSelfRel());
     }
 
     @GetMapping("/{id}")
     public EstadoHabitacionResponse findById(@PathVariable Long id) {
-        return estadoHabitacionService.findById(id);
+        return addLinks(estadoHabitacionService.findById(id));
     }
 
     @GetMapping("/habitacion/{numeroHabitacion}")
     public EstadoHabitacionResponse findByNumeroHabitacion(@PathVariable String numeroHabitacion) {
-        return estadoHabitacionService.findByNumeroHabitacion(numeroHabitacion);
+        return addLinks(estadoHabitacionService.findByNumeroHabitacion(numeroHabitacion));
     }
 
     @GetMapping("/estado/{estado}")
-    public List<EstadoHabitacionResponse> findByEstado(@PathVariable String estado) {
-        return estadoHabitacionService.findByEstado(estado);
+    public CollectionModel<EstadoHabitacionResponse> findByEstado(@PathVariable String estado) {
+        List<EstadoHabitacionResponse> list = estadoHabitacionService.findByEstado(estado);
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(EstadoHabitacionController.class).findByEstado(estado)).withSelfRel());
     }
 
     @GetMapping("/actualizado/{actualizadoEn}")
-    public List<EstadoHabitacionResponse> findByActualizadoEn(
+    public CollectionModel<EstadoHabitacionResponse> findByActualizadoEn(
             @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate actualizadoEn) {
-        return estadoHabitacionService.findByActualizadoEn(actualizadoEn);
+        List<EstadoHabitacionResponse> list = estadoHabitacionService.findByActualizadoEn(actualizadoEn);
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(EstadoHabitacionController.class).findByActualizadoEn(actualizadoEn)).withSelfRel());
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public EstadoHabitacionResponse create(@Valid @RequestBody EstadoHabitacionRequest request) {
-        return estadoHabitacionService.create(request);
+        return addLinks(estadoHabitacionService.create(request));
     }
 
     @PutMapping("/{id}")
     public EstadoHabitacionResponse update(
             @PathVariable Long id,
             @Valid @RequestBody EstadoHabitacionRequest request) {
-        return estadoHabitacionService.update(id, request);
+        return addLinks(estadoHabitacionService.update(id, request));
     }
 
     @PatchMapping("/habitacion/{numeroHabitacion}/estado")
     public EstadoHabitacionResponse cambiarEstado(
             @PathVariable String numeroHabitacion,
             @RequestParam String estado) {
-        return estadoHabitacionService.cambiarEstado(numeroHabitacion, estado);
+        return addLinks(estadoHabitacionService.cambiarEstado(numeroHabitacion, estado));
     }
 
     @DeleteMapping("/{id}")
