b8ae2fc7b088d0db2bb1e893738e948c9df782dc
constanza <co.barrag@duocuc.cl>
2026-06-29 17:55:22 -0400

    Add HATEOAS links to habitacion controller

--- ms-habitaciones/src/main/java/cl/hilton/habitaciones/controller/HabitacionController.java
+++ ms-habitaciones/src/main/java/cl/hilton/habitaciones/controller/HabitacionController.java
@@ -2,6 +2,7 @@ package cl.hilton.habitaciones.controller;
 
 import java.util.List;
 
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,6 +22,9 @@ import cl.hilton.habitaciones.service.HabitacionService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @RestController
 @RequestMapping("/api/v1/habitaciones")
 @RequiredArgsConstructor
@@ -28,61 +32,80 @@ public class HabitacionController {
 
     private final HabitacionService habitacionService;
 
+    private HabitacionResponse addLinks(HabitacionResponse h) {
+        h.add(linkTo(methodOn(HabitacionController.class).findById(h.getId())).withSelfRel());
+        h.add(linkTo(methodOn(HabitacionController.class).update(h.getId(), null)).withRel("update").withTitle("PUT - Actualizar habitacion"));
+        h.add(linkTo(methodOn(HabitacionController.class).deleteById(h.getId())).withRel("delete").withTitle("DELETE - Eliminar habitacion"));
+        h.add(linkTo(methodOn(HabitacionController.class).cambiarActiva(h.getId(), null)).withRel("cambiar-activa").withTitle("PATCH - Cambiar estado activa"));
+        h.add(linkTo(methodOn(HabitacionController.class).findAll()).withRel("all").withTitle("GET - Todas las habitaciones"));
+        return h;
+    }
+
     @GetMapping
-    public List<HabitacionResponse> findAll() {
-        return habitacionService.findAll();
+    public CollectionModel<HabitacionResponse> findAll() {
+        List<HabitacionResponse> list = habitacionService.findAll();
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(HabitacionController.class).findAll()).withSelfRel());
     }
 
     @GetMapping("/{id}")
     public HabitacionResponse findById(@PathVariable Long id) {
-        return habitacionService.findById(id);
+        return addLinks(habitacionService.findById(id));
     }
 
     @GetMapping("/numero/{numeroHabitacion}")
     public HabitacionResponse findByNumeroHabitacion(@PathVariable String numeroHabitacion) {
-        return habitacionService.findByNumeroHabitacion(numeroHabitacion);
+        return addLinks(habitacionService.findByNumeroHabitacion(numeroHabitacion));
     }
 
     @GetMapping("/piso/{piso}")
-    public List<HabitacionResponse> findByPiso(@PathVariable Integer piso) {
-        return habitacionService.findByPiso(piso);
+    public CollectionModel<HabitacionResponse> findByPiso(@PathVariable Integer piso) {
+        List<HabitacionResponse> list = habitacionService.findByPiso(piso);
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(HabitacionController.class).findByPiso(piso)).withSelfRel());
     }
 
     @GetMapping("/activas/{activa}")
-    public List<HabitacionResponse> findByActiva(@PathVariable Boolean activa) {
-        return habitacionService.findByActiva(activa);
+    public CollectionModel<HabitacionResponse> findByActiva(@PathVariable Boolean activa) {
+        List<HabitacionResponse> list = habitacionService.findByActiva(activa);
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(HabitacionController.class).findByActiva(activa)).withSelfRel());
     }
 
     @GetMapping("/tipo/{codigoTipo}")
-    public List<HabitacionResponse> findByCodigoTipo(@PathVariable String codigoTipo) {
-        return habitacionService.findByCodigoTipo(codigoTipo);
+    public CollectionModel<HabitacionResponse> findByCodigoTipo(@PathVariable String codigoTipo) {
+        List<HabitacionResponse> list = habitacionService.findByCodigoTipo(codigoTipo);
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(HabitacionController.class).findByCodigoTipo(codigoTipo)).withSelfRel());
     }
 
     @GetMapping("/tipo/{codigoTipo}/activa/{activa}")
-    public List<HabitacionResponse> findByCodigoTipoAndActiva(
+    public CollectionModel<HabitacionResponse> findByCodigoTipoAndActiva(
             @PathVariable String codigoTipo,
             @PathVariable Boolean activa) {
-        return habitacionService.findByCodigoTipoAndActiva(codigoTipo, activa);
+        List<HabitacionResponse> list = habitacionService.findByCodigoTipoAndActiva(codigoTipo, activa);
+        list.forEach(this::addLinks);
+        return CollectionModel.of(list, linkTo(methodOn(HabitacionController.class).findByCodigoTipoAndActiva(codigoTipo, activa)).withSelfRel());
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public HabitacionResponse create(@Valid @RequestBody HabitacionRequest request) {
-        return habitacionService.create(request);
+        return addLinks(habitacionService.create(request));
     }
 
     @PutMapping("/{id}")
     public HabitacionResponse update(
             @PathVariable Long id,
             @Valid @RequestBody HabitacionRequest request) {
-        return habitacionService.update(id, request);
+        return addLinks(habitacionService.update(id, request));
     }
 
     @PatchMapping("/{id}/activa")
     public HabitacionResponse cambiarActiva(
             @PathVariable Long id,
             @RequestParam Boolean activa) {
-        return habitacionService.cambiarActiva(id, activa);
+        return addLinks(habitacionService.cambiarActiva(id, activa));
     }
 
     @DeleteMapping("/{id}")
