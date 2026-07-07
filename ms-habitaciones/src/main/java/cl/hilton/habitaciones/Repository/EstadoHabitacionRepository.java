e05f32797b3498c7f6b88c5beb76d1bc6624068c
constanza <co.barrag@duocuc.cl>
2026-07-07 17:04:45 -0400

    Add estado cleanup method

--- ms-habitaciones/src/main/java/cl/hilton/habitaciones/Repository/EstadoHabitacionRepository.java
+++ ms-habitaciones/src/main/java/cl/hilton/habitaciones/Repository/EstadoHabitacionRepository.java
@@ -16,6 +16,8 @@ public interface EstadoHabitacionRepository extends JpaRepository<EstadoHabitaci
 
     boolean existsByHabitacionNumeroHabitacion(String numeroHabitacion);
 
+    void deleteByHabitacionNumeroHabitacion(String numeroHabitacion);
+
     List<EstadoHabitacion> findByEstado(String estado);
 
     List<EstadoHabitacion> findByActualizadoEn(LocalDate actualizadoEn);
